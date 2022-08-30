df0b57288cdd28a3245d7fc2c8cb3655b7c1aae9
avidalto <alvaro@parallelworks.com>
2022-08-30 20:21:51 +0000

    fix path to odcker-kill

--- jupyter-docker/kill-template.sh
+++ jupyter-docker/kill-template.sh
@@ -1,3 +1,12 @@
 
 job_number=__job_number__
-bash docker-kill-${job_number}.sh
+chdir=__chdir__
+
+if ! [ -z ${chdir} ] && ! [[ "${chdir}" == "default" ]]; then
+    chdir=$(echo ${chdir} | sed "s|__job_number__|${job_number}|g")
+    remote_session_dir=${chdir}
+else
+    remote_session_dir="./"
+fi
+
+bash ${remote_session_dir}/docker-kill-${job_number}.sh

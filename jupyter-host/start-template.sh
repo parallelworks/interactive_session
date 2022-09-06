e74c5e4cfd11ef003b98bd9eb142adf764a3ef76
avidalto <avidalto@hawk.iit.edu>
2022-09-06 21:41:50 +0000

    Fix jupyter-host!

--- jupyter-host/start-template.sh
+++ jupyter-host/start-template.sh
@@ -1,56 +1,28 @@
-echo "$(date): $(hostname):${PWD} $0 $@"
-
-path_to_sing="__path_to_sing__"
+# Runs via ssh + sbatch
 servicePort="__servicePort__"
-use_gpus="__use_gpus__"
+password="__password__"
 
-# MOUNT DIR DEFAULTS
-mount_dirs="${mount_dirs} -B ${HOME}:${HOME}"
-if [ -d "/contrib" ]; then
-    mount_dirs="${mount_dirs} -B /contrib:/contrib"
-fi
+echo "Activating conda..."
+# FIXME: THIS SHOULD BE AN INPUT!
+source __conda_sh__
+conda activate __conda_env__
+echo "Conda activated"
 
-if [ -d "/lustre" ]; then
-    mount_dirs="${mount_dirs} -B /lustre:/lustre"
-fi
+echo "starting notebook on $servicePort..."
 
-echo ${mdirs_cmd}
+export XDG_RUNTIME_DIR=""
 
+# Generate sha:
+echo "Generating sha"
+sha=$(python3 -c "from notebook.auth.security import passwd; print(passwd('${password}', algorithm = 'sha1'))")
 
-# GPU SUPPORT
-if [[ ${use_gpus} == "True" ]]; then
-    gpu_flag="--nv"
-else
-    gpu_flag=""
-fi
-
-
-# SANITY CHECKS!
-if ! [ -f "${path_to_sing}" ]; then
-    echo "ERROR: File $(hostname):${path_to_sing} not found!"
-    # FIXME: This error is not always streamed back
-    sleep 30
-    exit 1
-fi
-
-# WEB ADDRESS ISSUES:
-# https://github.com/rstudio/rstudio/issues/7953
-# https://support.rstudio.com/hc/en-us/articles/200552326-Running-RStudio-Server-with-a-Proxy
-
-
-password=EnterPasword
-singularity run ${gpu_flag} \
-    ${mount_dirs} \
-    ${path_to_sing} \
-    jupyter-notebook \
+jupyter-notebook \
     --port=$servicePort \
-    --ip=0.0.0.0 \
     --NotebookApp.iopub_data_rate_limit=10000000000 \
     --NotebookApp.token= \
-    --NotebookApp.password=$(python -c "from notebook.auth.security import passwd; print(passwd('__password__', algorithm = 'sha1'))") \
+    --NotebookApp.password=$sha \
     --no-browser \
     --notebook-dir=~/ \
     --NotebookApp.tornado_settings="{'static_url_prefix':'/${FORWARDPATH}/${IPADDRESS}/${openPort}/static/'}" \
     --NotebookApp.base_url="/${FORWARDPATH}/${IPADDRESS}/${openPort}/" \
     --NotebookApp.allow_origin=*
-

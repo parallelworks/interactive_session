3b686b8230ac5330cd0ad125dd899bb36d8ab300
avidalto <avidalto@hawk.iit.edu>
2022-09-14 18:19:45 +0000

    Add cleanup to webshell - Need to somehow generalize this to all services!

---
+++ webshell/kill-template.sh
@@ -0,0 +1,14 @@
+# Runs in the controller node:
+
+job_number=__job_number__
+chdir=__chdir__
+
+if ! [ -z ${chdir} ] && ! [[ "${chdir}" == "default" ]]; then
+    chdir=$(echo ${chdir} | sed "s|__job_number__|${job_number}|g")
+    remote_session_dir=${chdir}
+else
+    remote_session_dir="./"
+fi
+
+bash ${remote_session_dir}/service-kill-${job_number}.sh
+
--- webshell/start-template.sh
+++ webshell/start-template.sh
@@ -2,6 +2,32 @@
 
 partition_or_controller=__partition_or_controller__
 servicePort="__servicePort__"
+job_number=__job_number__
+job_dir=${PWD}
+
+
+# Prepare kill service script
+# - Needs to be here because we need the hostname of the compute node.
+# - kill-template.sh --> service-kill-${job_number}.sh --> service-kill-${job_number}-main.sh
+
+if [[ ${partition_or_controller} == "True" ]]; then
+    # Remove .cluster.local for einteinmed!
+    hname=$(hostname | sed "s/.cluster.local//g")
+    echo "ssh ${hname} 'bash -s' < ${PWD}/service-kill-${job_number}-main.sh" > service-kill-${job_number}.sh
+else
+    echo "bash ${PWD}/service-kill-${job_number}-main.sh" > service-kill-${job_number}.sh
+fi
+
+cat >> service-kill-${job_number}-main.sh <<HERE
+service_pid=\$(cat ${PWD}/service.pid)
+if [ -z \${service_pid} ]; then
+    echo "ERROR: No service pid was found!"
+else
+    echo "$(hostname) - Killing process: ${service_pid}"
+    pkill -P \${service_pid}
+    kill \${service_pid}
+fi
+HERE
 
 cd ~/
 
@@ -38,7 +64,17 @@ if ! [ -d "$(echo ~/pworks/noVNC-1.3.0)" ]; then
 fi
 chmod +x ./pworks/noVNC-1.3.0/ttyd.x86_64
 
-screen -wipe
-screen -S tty -d -m  ./pworks/noVNC-1.3.0/ttyd.x86_64 -p $servicePort bash
+#screen -wipe
+#screen -S tty -d -m  ./pworks/noVNC-1.3.0/ttyd.x86_64 -p $servicePort bash
+rm -rf ${job_dir}/service.pid
 
-sleep 9999
+if [ -z "$(which screen)" ]; then
+    ./pworks/noVNC-1.3.0/ttyd.x86_64 -p $servicePort bash &
+    echo $! >> ${job_dir}/service.pid
+else
+    screen -S tty-${job_number} -d -m ./pworks/noVNC-1.3.0/ttyd.x86_64 -p $servicePort bash
+    pid=$(ps -x | grep noVNC-${job_number} | grep -wv grep | awk '{print $1}')
+    echo ${pid} >> ${job_dir}/service.pid
+fi
+
+sleep 99999

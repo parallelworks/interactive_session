9a4fe5fe3e12b2b39027e5401d39b739627ed07c
Matthew Shaxted <mattshax@gmail.com>
2022-09-14 12:05:20 -0500

    Modifying the web shell workflow

--- webshell/start-template.sh
+++ webshell/start-template.sh
@@ -4,7 +4,39 @@ servicePort="__servicePort__"
 
 cd ~/
 
+# Check if the noVNC directory is present
+# - if not copy from user container -> /swift-pw-bin/noVNC-1.3.0.tgz
+if ! [ -d "~/pworks/noVNC-1.3.0" ]; then
+    echo "Bootstrapping noVNC"
+    set -x
+    mkdir -p ~/pworks
+    ssh_options="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+    if [[ ${partition_or_controller} == "True" ]]; then
+        # Running in a compute partition
+        if [[ "$USERMODE" == "k8s" ]]; then
+            # HAVE TO DO THIS FOR K8S NETWORKING TO EXPOSE THE PORT
+            # WARNING: Maybe if controller contains user name (user@ip) you need to extract only the ip
+            # Works because home directory is shared!
+            ssh ${ssh_options} $masterIp scp ${USER_CONTAINER_HOST}:/swift-pw-bin/noVNC-1.3.0.tgz ~/pworks
+        else # Docker mode
+            # Works because home directory is shared!
+            ssh ${ssh_options} $masterIp scp ${USER_CONTAINER_HOST}:/swift-pw-bin/noVNC-1.3.0.tgz ~/pworks
+        fi
+    else
+        # Running in a controller node
+        if [[ "$USERMODE" == "k8s" ]]; then
+            # HAVE TO DO THIS FOR K8S NETWORKING TO EXPOSE THE PORT
+            # WARNING: Maybe if controller contains user name (user@ip) you need to extract only the ip
+            scp ${USER_CONTAINER_HOST}:/swift-pw-bin/noVNC-1.3.0.tgz ~/pworks
+        else # Docker mode
+            scp ${USER_CONTAINER_HOST}:/swift-pw-bin/noVNC-1.3.0.tgz ~/pworks
+        fi
+    fi
+    tar -zxf ~/pworks/noVNC-1.3.0.tgz -C ~/pworks
+    set +x
+fi
+
 screen -wipe
-screen -S tty -d -m  ./pworks/ttyd.x86_64 -p $servicePort bash
+screen -S tty -d -m  ./pworks/noVNC-1.3.0/ttyd.x86_64 -p $servicePort bash
 
 sleep 9999

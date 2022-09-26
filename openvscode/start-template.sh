e3108ea04f2080467858da89767f9c465def674b
avidalto <avidalto@hawk.iit.edu>
2022-09-26 20:20:11 +0000

    Added cleanup for openvscode-server

--- openvscode/start-template.sh
+++ openvscode/start-template.sh
@@ -7,8 +7,27 @@ set -x
 # 3. install_paths
 
 server_exec=__server_exec__
+partition_or_controller=__partition_or_controller__
 install_paths="${HOME}/pworks/*/bin /opt/*/bin /shared/*/bin"
 
+# Prepare kill service script
+# - Needs to be here because we need the hostname of the compute node.
+# - kill-template.sh --> service-kill-${job_number}.sh --> service-kill-${job_number}-main.sh
+echo "Creating file ${chdir}/service-kill-${job_number}-main.sh from directory ${PWD}"
+if [[ ${partition_or_controller} == "True" ]]; then
+    # Remove .cluster.local for einteinmed!
+    hname=$(hostname | sed "s/.cluster.local//g")
+    echo "ssh ${hname} 'bash -s' < ${chdir}/service-kill-${job_number}-main.sh" > ${chdir}/service-kill-${job_number}.sh
+else
+    echo "bash ${chdir}/service-kill-${job_number}-main.sh" > ${chdir}/service-kill-${job_number}.sh
+fi
+
+cat >> ${chdir}/service-kill-${job_number}-main.sh <<HERE
+service_pid=\$(ps -x | grep openvscode-server | grep ${servicePort} | awk '{print $1}')
+HERE
+
+
+# START SERVICE
 if ! [ -z $(which openvscode-server) ]; then
     server_exec=$(which openvscode-server)
 elif [ -z ${server_exec} ] || [[ "${server_exec}" == "__""server_exec""__" ]]; then

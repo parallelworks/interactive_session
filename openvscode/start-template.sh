e0174b207e5ec15c351474912a9e45e66fb21c09
avidalto <avidalto@hawk.iit.edu>
2022-09-27 16:40:04 +0000

    Add bootstrap to openvscode service

--- openvscode/start-template.sh
+++ openvscode/start-template.sh
@@ -34,11 +34,51 @@ kill \${service_pid}
 pkill \${service_pid}
 HERE
 
+# BOOTSTRAP CODE --> FIXME: Cannot be generalized for different versions in different systems!
+install_dir=${HOME}/pworks/code-server-4.7.0-linux-amd64
+tgz_path=/swift-pw-bin/apps/code-server-4.7.0-linux-amd64.tar.gz
+# Check if the noVNC directory is present
+# - if not copy from user container -> /swift-pw-bin/noVNC-1.3.0.tgz
+if ! [ -d "${install_dir}" ]; then
+    echo "Bootstrapping ${install_dir}"
+    mkdir -p ~/pworks
+
+    # first check if the noVNC file is available on the node
+    if [[ -f "/core/pworks-main/${tgz_path}" ]]; then
+        cp /core/pworks-main/${tgz_path} ~/pworks
+    else
+        ssh_options="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+        if [[ ${partition_or_controller} == "True" ]]; then
+            # Running in a compute partition
+            if [[ "$USERMODE" == "k8s" ]]; then
+                # HAVE TO DO THIS FOR K8S NETWORKING TO EXPOSE THE PORT
+                # WARNING: Maybe if controller contains user name (user@ip) you need to extract only the ip
+                # Works because home directory is shared!
+                ssh ${ssh_options} $masterIp scp ${USER_CONTAINER_HOST}:${tgz_path} ~/pworks
+            else # Docker mode
+                # Works because home directory is shared!
+                ssh ${ssh_options} $masterIp scp ${USER_CONTAINER_HOST}:${tgz_path} ~/pworks
+            fi
+        else
+            # Running in a controller node
+            if [[ "$USERMODE" == "k8s" ]]; then
+                # HAVE TO DO THIS FOR K8S NETWORKING TO EXPOSE THE PORT
+                # WARNING: Maybe if controller contains user name (user@ip) you need to extract only the ip
+                scp ${USER_CONTAINER_HOST}:${tgz_path} ~/pworks
+            else # Docker mode
+                scp ${USER_CONTAINER_HOST}:${tgz_path} ~/pworks
+            fi
+        fi
+
+    fi
+    tar -zxf ~/pworks/$(basename ${tgz_path}) -C ~/pworks
+fi
+
+
 # SET DEFAULTS:
 if [ -z ${server_dir} ] || [[ "${server_dir}" == "__""server_dir""__" ]]; then
     server_dir=~/
 fi
-
 
 # START SERVICE
 if ! [ -z $(which ${server_bin}) ]; then
@@ -57,6 +97,8 @@ ${server_exec} \
     --bind-addr=localhost:${servicePort} \
     ${server_dir}
 
+# DELETE ME: To test different server commands
+sleep 99999
 
 exit 0
 # For openvscode-server

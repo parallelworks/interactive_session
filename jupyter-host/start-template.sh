f992fee6b4b7b8e6cdb185c6d067fb5624760320
avidalto <avidalto@hawk.iit.edu>
2022-10-26 16:04:20 -0500

    Add support for slurm modules to jupyter-host and xml for podmt3

--- jupyter-host/start-template.sh
+++ jupyter-host/start-template.sh
@@ -1,33 +1,44 @@
 # Runs via ssh + sbatch
+set -x
 
 password="__password__"
 notebook_dir="__notebook_dir__"
 CONDA_PATH=$(echo __conda_sh__ | sed "s|etc/profile.d/conda.sh||g")
+conda_sh=__conda_sh__
+conda_env=__conda_env__
+slurm_module=__slurm_module__
 
 CONDA_DIR="$(basename $CONDA_PATH)"
 INSTALL_DIR="$(dirname $CONDA_PATH)"
 
 if [ ! -d "$CONDA_PATH" ] && [ "__conda_install__" == "True" ];then
-
+    
     echo "No conda environment found - provisioning miniconda the first time..."
-
+    
     mkdir -p $INSTALL_DIR
     wget -O $INSTALL_DIR/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash $INSTALL_DIR/miniconda.sh -b -p $CONDA_PATH
     rm $INSTALL_DIR/miniconda.sh -f
-
+    
     source $CONDA_PATH/etc/profile.d/conda.sh
     conda activate __conda_env__
     conda install -c anaconda jupyter
-
+    
     echo "Conda now installed..."
-
+    
 fi
 
-echo "Activating conda..."
-source __conda_sh__
-conda activate __conda_env__
-echo "Conda activated"
+if [ -z "${conda_sh}" ] || [[ "${conda_sh}" == "__""conda_sh""__" ]]; then
+    source ${conda_sh}
+fi
+
+if [ -z "${conda_env}" ] || [[ "${conda_env}" == "__""conda_env""__" ]]; then
+    conda activate ${conda_env}
+fi
+
+if [ -z "${slurm_module}" ] || [[ "${slurm_module}" == "__""slurm_module""__" ]]; then
+    module load ${slurm_module}
+fi
 
 echo "starting notebook on $servicePort..."
 
@@ -48,15 +59,14 @@ if [ -z ${notebook_dir} ] || [[ "${notebook_dir}" == "__""notebook_dir""__" ]]; 
     notebook_dir="/"
 fi
 
-set -x
 jupyter-notebook \
-    --port=$servicePort \
-    --ip=0.0.0.0 \
-    --NotebookApp.iopub_data_rate_limit=10000000000 \
-    --NotebookApp.token= \
-    --NotebookApp.password=$sha \
-    --no-browser \
-    --notebook-dir=$notebook_dir \
-    --NotebookApp.tornado_settings="{'static_url_prefix':'/${FORWARDPATH}/${IPADDRESS}/${openPort}/static/'}" \
-    --NotebookApp.base_url="/${FORWARDPATH}/${IPADDRESS}/${openPort}/" \
-    --NotebookApp.allow_origin=*
+--port=$servicePort \
+--ip=0.0.0.0 \
+--NotebookApp.iopub_data_rate_limit=10000000000 \
+--NotebookApp.token= \
+--NotebookApp.password=$sha \
+--no-browser \
+--notebook-dir=$notebook_dir \
+--NotebookApp.tornado_settings="{'static_url_prefix':'/${FORWARDPATH}/${IPADDRESS}/${openPort}/static/'}" \
+--NotebookApp.base_url="/${FORWARDPATH}/${IPADDRESS}/${openPort}/" \
+--NotebookApp.allow_origin=*

41d056c1cbf8a7892ec5a23029a394faa723b41e
avidalto <avidalto@hawk.iit.edu>
2022-10-28 13:20:04 -0500

    Sync all files

--- platforms/einsteinmed-user-1.parallel.works/env.sh
+++ platforms/einsteinmed-user-1.parallel.works/env.sh
@@ -1,6 +1,6 @@
 export CONDA_PYTHON_EXE=/public/apps/conda3/bin/python3
 
 mkdir -p  ~/pw/bootstrap
-rsync /swift-pw-bin/apps/*.tgz ~/pw/bootstrap
+rsync /swift-pw-bin/apps/* ~/pw/bootstrap
 
 export USER_CONTAINER_HOST=${PW_USER_HOST} 

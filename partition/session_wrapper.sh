72356ddd96bf8df1597ea4235fc4d14eaaf8c994
avidalto <avidalto@hawk.iit.edu>
2022-10-28 11:55:10 -0500

    Remove debug for kill.sh

--- partition/session_wrapper.sh
+++ partition/session_wrapper.sh
@@ -192,7 +192,6 @@ fi
 echo $sshcmd scancel $slurmjob >> ${kill_sh}
 echo "echo Finished running ${kill_sh}" >> ${kill_sh}
 echo "sed -i 's/.*Job status.*/Job status: Cancelled/' /pw/jobs/${job_number}/service.html"  >> ${kill_sh}
-echo \${PWD}   >> ${kill_sh}
 chmod 777 ${kill_sh}
 
 echo

6ce8b6421b2bae48c7b77553a1a978120ba4ff70
avidalto <avidalto@hawk.iit.edu>
2022-11-04 16:50:18 -0500

    Move gnome-session before xfce4-session

--- turbovnc/start-template.sh
+++ turbovnc/start-template.sh
@@ -153,13 +153,13 @@ touch ${chdir}/service.pid
 if ! [ -z $(which mate-session) ]; then
     mate-session &
     echo $! > ${chdir}/service.pid
+elif  ! [ -z $(which gnome-session) ]; then
+    gnome-session &
+    echo $! > ${chdir}/service.pid
 elif ! [ -z $(which xfce4-session) ]; then
     # WARNING! NEEDS TO GO BEFORE GNOME BECAUSE INSTALLING XFCE FOR SOME REASON INSTALLS GNOME-SESSION
     #          BUT THE COMMAND GNOME-SESSION FAILS!
     xfce4-session &
-    echo $! > ${chdir}/service.pid
-elif  ! [ -z $(which gnome-session) ]; then
-    gnome-session &
     echo $! > ${chdir}/service.pid
 elif ! [ -z $(which gnome) ]; then
     gnome &

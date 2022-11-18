72bab56b258b819ebe616d04e9bb8532c2771c77
avidalto <avidalto@hawk.iit.edu>
2022-11-18 12:54:57 -0600

    Add matlab-docker service

---
+++ matlab-docker/kill-template.sh
@@ -0,0 +1,17 @@
+
+job_number=__job_number__
+chdir=__chdir__
+
+if ! [ -z ${chdir} ] && ! [[ "${chdir}" == "default" ]]; then
+    chdir=$(echo ${chdir} | sed "s|__job_number__|${job_number}|g")
+    remote_session_dir=${chdir}
+else
+    remote_session_dir="./"
+fi
+
+{
+    bash ${remote_session_dir}/docker-kill-${job_number}.sh
+    rm ${remote_session_dir}/docker-kill-${job_number}.sh
+} || {
+    echo "ERROR: Could not run bash ${remote_session_dir}/docker-kill-${job_number}.sh. Please run it manually!"
+}
---
+++ matlab-docker/start-template.sh
@@ -0,0 +1,36 @@
+echo "$(date): $(hostname):${PWD} $0 $@"
+
+job_number=__job_number__
+partition_or_controller=__partition_or_controller__
+
+if [[ __use_gpus__ == "True" ]]; then
+    gpu_flag="--gpus all"
+else
+    gpu_flag=""
+fi
+
+if [[ ${partition_or_controller} == "True" ]]; then
+    # Create kill script. Needs to be here because we need the hostname of the compute node.
+    echo ssh "'$(hostname)'" sudo -n docker stop jupyter-$servicePort > docker-kill-${job_number}.sh
+else
+    echo sudo -n docker stop jupyter-$servicePort > docker-kill-${job_number}.sh
+fi
+
+chmod 777 docker-kill-${job_number}.sh
+
+sudo -n systemctl start docker
+
+
+# Docker supports mounting directories that do not exist (singularity does not)
+set -x
+
+# https://docs.docker.com/config/containers/container-networking/
+#    sudo docker run -it --rm -p 8888:8888 --shm-size=512M mathworks/matlab:r2022a -browser
+
+sudo -n docker run ${gpu_flag} -it --rm \
+    -v /contrib:/contrib -v /lustre:/lustre -v ${HOME}:${HOME} \
+    --name=matlab-$servicePort \
+    -p 8888:$servicePort \
+    --shm-size=512M \
+    __docker_repo__ \
+    -browser 

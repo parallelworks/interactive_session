bac8cdeed133e55b6befb285dc764376e423637f
Eric Garcia <egarcia@parallelworks.com>
2022-12-07 14:38:22 -0600

    change forward url

--- jupyter-host/url.sh
+++ jupyter-host/url.sh
@@ -2,9 +2,9 @@ export URLEND="tree?dt=\"+(new Date()).getTime()"
 #export URLEND="tree/home/${PW_USER}\""
 
 if [[ "$USERMODE" == "k8s" ]];then
-    export FORWARDPATH="pwide-kube-nb"
+    export FORWARDPATH="me"
     export IPADDRESS="$(hostname -I | xargs)"
 else
-    export FORWARDPATH="pwide-nb"
+    export FORWARDPATH="me"
     export IPADDRESS="$PW_USER_HOST"
 fi

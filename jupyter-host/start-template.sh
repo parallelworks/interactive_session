21f7afb7711969c8eb0bb33b667cf8f497f94df2
avidalto <avidalto@hawk.iit.edu>
2022-12-12 11:14:33 -0600

    Modify jupyter-host for newusercontainer

--- jupyter-host/start-template.sh
+++ jupyter-host/start-template.sh
@@ -59,30 +59,75 @@ if [ -z ${notebook_dir} ] || [[ "${notebook_dir}" == "__""notebook_dir""__" ]]; 
     notebook_dir="/"
 fi
 
+# Custom PW plugin:
+cat >> __init__.py <<HERE
+from tornado.web import StaticFileHandler
+from tornado import web
+import os
+from notebook.utils import url_path_join
+import pprint as pp
+
+def load_jupyter_server_extension(nbapp):
+    
+    print('loading custom plugin')
+
+    web_app = nbapp.web_app
+    base_url = web_app.settings['base_url']
+
+    static_path = web_app.settings.get("static_path")
+    path_join = url_path_join(base_url, '', 'static', '(.*)')
+
+    service_port = os.environ['SERVICE_PORT']
+    web_app.settings['base_url'] = '/me/%s/' % service_port
+
+    # pp.pprint(web_app.settings)
+
+    handlers = [
+         (
+            path_join,
+            StaticFileHandler,
+            {'path': os.path.join(static_path[0])}
+        )
+    ]
+    web_app.settings['nbapp'] = nbapp
+    web_app.add_handlers('.*', handlers)
+HERE
 
 if [[ "$NEW_USERCONTAINER" == "0" ]];then
     # Served from 
     # https://cloud.parallel.works/api/v2/proxy/usercontainer?proxyType=api&proxyTo=/api/v1/display/pw/jobs/57147/service.html
-    tornado_settings="{'static_url_prefix':'/me/${openPort}/static/'}"
-    base_url="/me/${openPort}/"
+    export PYTHONPATH=${PWD}
+    jupyter-notebook \
+        --port=${servicePort} \
+        --ip=0.0.0.0 \
+        --NotebookApp.default_url="/me/${openPort}/tree" \
+        --NotebookApp.iopub_data_rate_limit=10000000000 \
+        --NotebookApp.token= \
+        --NotebookApp.password=$sha \
+        --no-browser \
+        --notebook-dir=~/ \
+        --NotebookApp.nbserver_extensions "pw_jupyter_proxy=True" \
+        --NotebookApp.tornado_settings="{\"static_url_prefix\":\"/me/${openPort}/static/\"}" \
+        --NotebookApp.allow_origin=*
+
 else
     # Served from:
     # https://noaa.parallel.works /pwide-nb/noaa-user-1.parallel.works/50359/ tree?dt=1670280530105
     # https://cloud.parallel.work /api/v2/proxy/usercontainer?proxyType=api&proxyTo=/api/v1/display/pw/jobs/57147/ service.html
     tornado_settings="{'static_url_prefix':'/${FORWARDPATH}/${IPADDRESS}/${openPort}/static/'}"
     base_url="/${FORWARDPATH}/${IPADDRESS}/${openPort}/"
+    jupyter-notebook \
+        --port=$servicePort \
+        --ip=0.0.0.0 \
+        --NotebookApp.iopub_data_rate_limit=10000000000 \
+        --NotebookApp.token= \
+        --NotebookApp.password=$sha \
+        --no-browser \
+        --notebook-dir=$notebook_dir \
+        --NotebookApp.tornado_settings="{'static_url_prefix':'/${FORWARDPATH}/${IPADDRESS}/${openPort}/static/'}" \
+        --NotebookApp.base_url="/${FORWARDPATH}/${IPADDRESS}/${openPort}/" \
+        --NotebookApp.allow_origin=*
+    
 fi
-
-jupyter-notebook \
---port=$servicePort \
---ip=0.0.0.0 \
---NotebookApp.iopub_data_rate_limit=10000000000 \
---NotebookApp.token= \
---NotebookApp.password=$sha \
---no-browser \
---notebook-dir=$notebook_dir \
---NotebookApp.tornado_settings=${tornado_settings} \
---NotebookApp.base_url=${base_url} \
---NotebookApp.allow_origin=*
 
 sleep 9999

3dad6ee36918da83030f3b00606387b0dfc75dda
avidalto <avidalto@hawk.iit.edu>
2022-12-12 12:03:34 -0600

    Update docker and jupyter

--- jupyter-docker/start-template.sh
+++ jupyter-docker/start-template.sh
@@ -30,13 +30,85 @@ else
     sha=$(sudo -n docker run --rm __docker_repo__ python3 -c "from notebook.auth.security import passwd; print(passwd('__password__', algorithm = 'sha1'))")
 fi
 
+
+# Custom PW plugin:
+mkdir -p pw_jupyter_proxy
+cat >> pw_jupyter_proxy/__init__.py <<HERE
+from tornado.web import StaticFileHandler
+from tornado import web
+import os
+from notebook.utils import url_path_join
+import pprint as pp
+
+def load_jupyter_server_extension(nbapp):
+    
+    print('loading custom plugin')
+
+    web_app = nbapp.web_app
+    base_url = web_app.settings['base_url']
+
+    static_path = web_app.settings.get("static_path")
+    path_join = url_path_join(base_url, '', 'static', '(.*)')
+
+    web_app.settings['base_url'] = '/me/%s/' % ${openPort}
+
+    # pp.pprint(web_app.settings)
+
+    handlers = [
+         (
+            path_join,
+            StaticFileHandler,
+            {'path': os.path.join(static_path[0])}
+        )
+    ]
+    web_app.settings['nbapp'] = nbapp
+    web_app.add_handlers('.*', handlers)
+HERE
+
 if [[ "$NEW_USERCONTAINER" == "0" ]];then
-    tornado_settings="{'static_url_prefix':'/me/${openPort}/static/'}"
-    base_url="/me/${openPort}/"
+    # Served from 
+    # https://cloud.parallel.works/api/v2/proxy/usercontainer?proxyType=api&proxyTo=/api/v1/display/pw/jobs/57147/service.html
+    export PYTHONPATH=${PWD}
+    sudo -n docker run ${gpu_flag} --rm \
+        -v /contrib:/contrib -v /lustre:/lustre -v ${HOME}:${HOME} \
+        --name=jupyter-$servicePort \
+        -p $servicePort:$servicePort \
+        __docker_repo__ jupyter-notebook \
+            --port=${servicePort} \
+            --ip=0.0.0.0 \
+            --NotebookApp.default_url="/me/${openPort}/tree" \
+            --NotebookApp.iopub_data_rate_limit=10000000000 \
+            --NotebookApp.token= \
+            --NotebookApp.password=$sha \
+            --no-browser \
+            --notebook-dir=$notebook_dir \
+            --NotebookApp.nbserver_extensions "pw_jupyter_proxy=True" \
+            --NotebookApp.tornado_settings="{\"static_url_prefix\":\"/me/${openPort}/static/\"}" \
+            --NotebookApp.allow_origin=*
+
 else
-    tornado_settings="{'static_url_prefix':'/${FORWARDPATH}/${IPADDRESS}/${openPort}/static/'}"
-    base_url="/${FORWARDPATH}/${IPADDRESS}/${openPort}/"
+    # Served from:
+    # https://noaa.parallel.works /pwide-nb/noaa-user-1.parallel.works/50359/ tree?dt=1670280530105
+    # https://cloud.parallel.work /api/v2/proxy/usercontainer?proxyType=api&proxyTo=/api/v1/display/pw/jobs/57147/ service.html
+    sudo -n docker run ${gpu_flag} --rm \
+        -v /contrib:/contrib -v /lustre:/lustre -v ${HOME}:${HOME} \
+        --name=jupyter-$servicePort \
+        -p $servicePort:$servicePort \
+        __docker_repo__ jupyter-notebook \
+            --port=$servicePort \
+            --ip=0.0.0.0 \
+            --NotebookApp.iopub_data_rate_limit=10000000000 \
+            --NotebookApp.token= \
+            --NotebookApp.password=$sha \
+            --no-browser \
+            --notebook-dir=$notebook_dir \
+            --NotebookApp.tornado_settings="{'static_url_prefix':'/${FORWARDPATH}/${IPADDRESS}/${openPort}/static/'}" \
+            --NotebookApp.base_url="/${FORWARDPATH}/${IPADDRESS}/${openPort}/" \
+            --NotebookApp.allow_origin=*
+    
 fi
+
+sleep 9999
 
 # Docker supports mounting directories that do not exist (singularity does not)
 set -x
--- jupyter-host/start-template.sh
+++ jupyter-host/start-template.sh
@@ -114,8 +114,6 @@ else
     # Served from:
     # https://noaa.parallel.works /pwide-nb/noaa-user-1.parallel.works/50359/ tree?dt=1670280530105
     # https://cloud.parallel.work /api/v2/proxy/usercontainer?proxyType=api&proxyTo=/api/v1/display/pw/jobs/57147/ service.html
-    tornado_settings="{'static_url_prefix':'/${FORWARDPATH}/${IPADDRESS}/${openPort}/static/'}"
-    base_url="/${FORWARDPATH}/${IPADDRESS}/${openPort}/"
     jupyter-notebook \
         --port=$servicePort \
         --ip=0.0.0.0 \

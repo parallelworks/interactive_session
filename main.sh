dbb0741a792448726743aee43c002d7e34cfdbdb
avidalto <avidalto@hawk.iit.edu>
2022-12-20 06:17:17 -0600

    Get internal IP from the network internal name if specified

--- main.sh
+++ main.sh
@@ -124,6 +124,12 @@ if [ -z "${controller}" ]; then
 fi
 
 # GET INTERNAL IP OF CONTROLLER NODE. 
+poolInternalNetworkName=$(${CONDA_PYTHON_EXE} ${PWD}/utils/pool_api.py ${poolname} internalNetworkName)
+if [ -z "${poolInternalNetworkName}" ]; then
+    export masterIp=$($sshcmd hostname -I | cut -d' ' -f1) # Matthew: Master ip would usually be the internal ip
+else
+    export masterIp=$($sshcmd "ifconfig eth1" | grep inet | head -n1 | awk '{print $2}')
+fi
 # Compute needs to be able to access controller node through this  IP address with SSH! (ssh masterIp)
 export masterIp=$($sshcmd hostname -I | cut -d' ' -f1) # Matthew: Master ip would usually be the internal ip
 if [ -z ${masterIp} ]; then

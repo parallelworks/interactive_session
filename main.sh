ab9fe95a7b338fdd80a2a8500a63e10aaf2c8fa5
avidalto <avidalto@hawk.iit.edu>
2022-12-20 02:06:57 -0600

    Need to move files from workflow directory to avoid updating the sparse checkout

--- main.sh
+++ main.sh
@@ -17,8 +17,8 @@ echo
 
 # change permissions of run directly so we can execute all files
 chmod 777 * -Rf
-# Need to move files from workflow directory to avoid updating the sparse checkout
-mv workflow/error.html .
+# Need to move files from utils directory to avoid updating the sparse checkout
+mv utils/error.html .
 
 # Replace special placeholder since \$(whoami) and \${PW_USER} don't work everywhere and ${job_number} is not known
 # Preserve single quota (--pname 'pval') with ${@@Q}

0e369245b6acbc67603bd4ed8a10a0241b6f2c4a
avidalto <avidalto@hawk.iit.edu>
2023-01-20 10:41:30 -0600

    Emed no works with usercontainer

--- platforms/einsteinmed-user-1.parallel.works/env.sh
+++ platforms/einsteinmed-user-1.parallel.works/env.sh
@@ -3,8 +3,6 @@ export CONDA_PYTHON_EXE=/public/apps/conda3/bin/python3
 mkdir -p  ~/pw/bootstrap
 rsync /swift-pw-bin/apps/* ~/pw/bootstrap
 
-export USER_CONTAINER_HOST=${PW_USER_HOST} 
-
 # Separate multiple lines with ;#
 export RUNTIME_FIXES="source ~/.bashrc"
 

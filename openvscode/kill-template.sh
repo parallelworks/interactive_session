e7596e41ba0e0afec0b5b0774be7721c015a432a
avidalto <avidalto@hawk.iit.edu>
2023-05-23 11:04:13 -0500

    Fix bugs and clean killing script for controller jobs

--- openvscode/kill-template.sh
+++ openvscode/kill-template.sh
@@ -1,11 +1,4 @@
 
 # Runs in the controller node:
-if ! [ -z ${chdir} ] && ! [[ "${chdir}" == "default" ]]; then
-    chdir=$(echo ${chdir} | sed "s|__job_number__|${job_number}|g")
-    remote_session_dir=${chdir}
-else
-    remote_session_dir="./"
-fi
-
 bash ${remote_session_dir}/service-kill-${job_number}.sh
 

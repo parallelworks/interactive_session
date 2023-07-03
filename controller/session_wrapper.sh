e99031d3351c9e0584bcc984143fdc94b779d23d
Michael McQuade <michael@giraffesyo.io>
2023-07-03 11:57:58 -0500

    Add notification when workflow is running
    Co-authored-by: Eric Garcia <egarcia@parallelworks.com>

--- controller/session_wrapper.sh
+++ controller/session_wrapper.sh
@@ -137,6 +137,13 @@ echo "$sshcmd 'bash -s' < ${session_sh} &> ${PW_JOB_PATH}/session-${job_number}.
 echo
 sed -i 's/.*Job status.*/Job status: Running/' service.html
 sed -i "s/.*JOB_STATUS.*/    \"JOB_STATUS\": \"Running\",/" service.json
+job_dir=$(pwd | rev | cut -d'/' -f1-2 | rev)
+workflow_name=$(echo ${job_dir} | cut -d'/' -f1)
+job_number=$(echo ${job_dir} | cut -d'/' -f2)
+url="/workflows/${workflow_name}/${job_number}/view"
+curl -X POST -H "Content-Type: application/json" \
+    -d "{\"title\": \"Interactive workflow ${workflow_name} job ${job_number} is running\", \"href\": \"${url}\"}" \
+    https://${PW_PLATFORM_HOST}/api/v2/notifications
 $sshcmd 'bash -s' < ${session_sh} &> ${PW_JOB_PATH}/session-${job_number}.out
 
 if [ $? -eq 0 ]; then

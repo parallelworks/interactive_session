a1d0f2f56578146d76ee484f26b14faf00870f4c
avidalto <avidalto@hawk.iit.edu>
2023-08-30 10:14:43 -0500

    restart scw service after editing config XML

--- scw/start-template.sh
+++ scw/start-template.sh
@@ -1,6 +1,78 @@
-# Make sure no conda environment is activated! 
-# https://github.com/parallelworks/issues/issues/1081
 set -x
+
+# Stop SCW service
+"/c/Program Files/Penguin Computing/Scyld Cloud Workstation/bin/scyld-cloud-workstation.exe" /service=stop
+
+# Rewrite config file
+cat > "/c/Program Files/Penguin Computing/Scyld Cloud Workstation/scyld-cloud-workstation.xml" << END
+<config>
+  <Server>
+    <!-- <AutoLock>false</AutoLock> -->
+    <!-- <IdleUserTimeout>120</IdleUserTimeout> -->
+    <LicenseFile>27002@${resource_privateIp}</LicenseFile>
+    <!-- <LocalCursor>true</LocalCursor> -->
+    <!-- <LogLevel>information</LogLevel> -->
+    <PathPrefix></PathPrefix>
+    <Port>${servicePort}</Port>
+    <Security>
+      <SameOriginHeaders>disabled</SameOriginHeaders>
+    </Security>
+    <RedirectHTTPPort>false</RedirectHTTPPort>
+    <Secure>false</Secure>
+    <!-- <VideoSource>auto</VideoSource> -->
+    <Audio>
+      <!-- <Enabled>true</Enabled> -->
+    </Audio>
+    <Auth>
+      <!-- <MinPasswordLength>6</MinPasswordLength> -->
+      <!-- <OSAuthEnabled>true</OSAuthEnabled> -->
+      <Username>admin</Username>
+      <ScyldCloudAuth>
+        <!-- <URL></URL> -->
+        <Allow>
+          <!-- <Username></Username> -->
+        </Allow>
+        <Deny>
+          <!-- <Username></Username> -->
+        </Deny>
+      </ScyldCloudAuth>
+      <!-- <Enabled>false</Enabled> -->
+    </Auth>
+    <Broker>
+      <Username>broker</Username>
+    </Broker>
+    <Video>
+      <!-- <MaxHeight>1440</MaxHeight> -->
+      <!-- <MaxWidth>2560</MaxWidth> -->
+      <Encoding>
+        <H264>
+          <!-- <AvgBitRate>1280x720=3000k,1920x1080=6000k</AvgBitRate> -->
+          <!-- <MaxFrameRate>30</MaxFrameRate> -->
+        </H264>
+      </Encoding>
+    </Video>
+  </Server>
+  <openSSL>
+    <server>
+      <certificateFile>defaultCert.pem</certificateFile>
+      <privateKeyFile>defaultKey.pem</privateKeyFile>
+      <!-- <requireTLSv1_2>true</requireTLSv1_2> -->
+      <invalidCertificateHandler>
+        <!-- <name>RejectCertificateHandler</name> -->
+      </invalidCertificateHandler>
+      <privateKeyPassphraseHandler>
+        <options>
+          <!-- <password>secretsecret</password> -->
+        </options>
+      </privateKeyPassphraseHandler>
+    </server>
+  </openSSL>
+</config>
+END
+
+
+# Start SCW service
+"/c/Program Files/Penguin Computing/Scyld Cloud Workstation/bin/scyld-cloud-workstation.exe" /service=start
 
 echo "starting SCW on port $servicePort"
 

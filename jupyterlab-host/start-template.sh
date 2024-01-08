ca7d40c65de63b38be4a87ea7ecf9a8389a3932d
avidalto <avidalto@hawk.iit.edu>
2024-01-08 07:51:31 -0600

    Create conda environment if it does not exist

--- jupyterlab-host/start-template.sh
+++ jupyterlab-host/start-template.sh
@@ -40,6 +40,12 @@ f_set_up_conda_from_yaml() {
     
     echo "Sourcing Conda SH <${CONDA_SH}>"
     source ${CONDA_SH}
+    # Check if Conda environment exists
+    if ! conda env list | grep -q "${CONDA_ENV}"; then
+        echo "Creating Conda Environment <${CONDA_ENV}>"
+        conda create --name ${CONDA_ENV} [additional_packages]
+    fi
+    
     echo "Activating Conda Environment <${CONDA_ENV}>"
     conda activate ${CONDA_ENV}
     conda env update -n ${CONDA_ENV} -q -f ${CONDA_YAML}

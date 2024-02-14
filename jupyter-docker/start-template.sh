edc4dd47116c4a35ae56707c7a6e18dddca62d57
Alvaro Vidal Torreira <avidalto@hawk.iit.edu>
2024-02-14 08:50:58 -0600

    Update start-template.sh

--- jupyter-docker/start-template.sh
+++ jupyter-docker/start-template.sh
@@ -18,6 +18,12 @@ fi
 
 if [[ ${service_use_gpus} == "true" ]]; then
     gpu_flag="--gpus all"
+    # FIXME: This should go to the image creation
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+    sudo yum-config-manager --enable nvidia-container-toolkit-experimental
+    sudo yum install -y nvidia-container-toolkit
+    sudo nvidia-ctk runtime configure --runtime=docker
+    sudo systemctl restart docker
 else
     gpu_flag=""
 fi

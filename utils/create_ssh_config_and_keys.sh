60f10e57d58954ea572044163a1f81daa6ee42f5
Alvaro Vidal Torreira <avidalto@hawk.iit.edu>
2024-02-22 13:25:09 -0600

    Update create_ssh_config_and_keys.sh

--- utils/create_ssh_config_and_keys.sh
+++ utils/create_ssh_config_and_keys.sh
@@ -40,5 +40,6 @@ Host usercontainer
 
 HERE
 
-rm -f ${HOME}/.ssh/id_rsa*
-ssh-keygen -t rsa -N "" -q -f  ${HOME}/.ssh/id_rsa
+if ! [ -f "${HOME}/.ssh/id_rsa" ]; then
+    ssh-keygen -t rsa -N "" -q -f  ${HOME}/.ssh/id_rsa
+fi

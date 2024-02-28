77a4df8278604db29b143d0bba68d514748e0d2c
avidalto <avidalto@hawk.iit.edu>
2024-02-28 11:57:04 -0600

    Fix displayErrorMessage function to change the job status to FAILED in the service.json

--- lib.sh
+++ lib.sh
@@ -6,7 +6,7 @@ echod() {
 displayErrorMessage() {
     echo $(date): $1
     sed -i "s/.*ERROR_MESSAGE.*/    \"ERROR_MESSAGE\": \"$1\"/" service.json
-    sed -i "s/.*JOB_STATUS.*/    \"JOB_STATUS\": \"FAILED,\"/" service.json
+    sed -i "s/.*JOB_STATUS.*/    \"JOB_STATUS\": \"FAILED\",/" service.json
     exit 1
 }
 

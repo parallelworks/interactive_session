7e4d0f427fd0fedbbab369200b810037c6f2f605
Alvaro Vidal Torreira <avidalto@hawk.iit.edu>
2024-03-25 12:08:00 -0500

    Add group to inputs.sh/inputs.json for GT workflow

--- utils/input_form_resource_wrapper.py
+++ utils/input_form_resource_wrapper.py
@@ -412,6 +412,7 @@ def complete_resource_information(inputs_dict):
         resource_info = get_resource_info_with_verified_ip(resource_id)
         public_ip = get_resource_external_ip(resource_info)
 
+	inputs_dict['resource']['group'] = resource_info['group']
         inputs_dict['resource']['publicIp'] = public_ip
         inputs_dict['resource']['username'] = get_resource_user(resource_info)
         inputs_dict['resource']['type'] = resource_info['type']

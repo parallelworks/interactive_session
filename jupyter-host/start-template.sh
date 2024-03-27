4a6b56f8c5cb1c5a30aba45feb02807331fc1f5e
avidalto <avidalto@hawk.iit.edu>
2024-03-27 11:42:55 -0500

    Fix docker issue

--- jupyter-host/start-template.sh
+++ jupyter-host/start-template.sh
@@ -210,8 +210,8 @@ else
     touch empty
     sudo docker run  -d --name ${container_name}  \
         -v $PWD/config.conf:/etc/nginx/conf.d/config.conf \
-        -v empty:/etc/nginx/conf.d/default.conf \
-        --network=host nginxinc/nginx-unprivileged
+        -v ${PWD}/empty:/etc/nginx/conf.d/default.conf \
+        --network=host nginxinc/nginx-unprivileged:1.25.3
     # Print logs
     sudo docker logs ${container_name}
 fi

b55941a9afa34bd60042f31f31edd246ea320de9
Louis Le <lle@parallelworks.com>
2024-07-31 16:54:36 -0400

    check if this one is running on partition

--- partition/session_wrapper.sh
+++ partition/session_wrapper.sh
@@ -146,6 +146,7 @@ scp stream.sh ${resource_publicIp}:${resource_jobdir}/stream-${job_number}.sh
 
 echo
 echo "Submitting ${submit_cmd} request (wait for node to become available before connecting)..."
+echo "Louis test partition"
 echo
 echo $sshcmd ${submit_cmd} ${resource_jobdir}/session-${job_number}.sh
 

dc32166730b88db702ad415b55792a26e7de4905
daveisdigital <dave@adaptable.sh>
2024-08-02 11:48:41 -0500

    Brute-force the dbus restart regardless of which OS

--- turbovnc/start-template.sh
+++ turbovnc/start-template.sh
@@ -1,14 +1,10 @@
 # Make sure no conda environment is activated! 
 # https://github.com/parallelworks/issues/issues/1081
 
-if [[ "$resource_type" != slurm* && "$resource_type" != "existing" ]]; then
-	if grep -q "Rocky Linux release 8" /etc/redhat-release; then
-		if ! [ -f /var/tmp/dbus.restart ]; then
-    		echo Restarting dbus daemon 
-    		sudo systemctl restart dbus
-    		touch /var/tmp/dbus.restart
-		fi
-	fi
+if ! [ -f /var/tmp/dbus.restart ]; then
+	echo Restarting dbus daemon 
+    	sudo systemctl restart dbus
+    	touch /var/tmp/dbus.restart
 fi
 
 # Determine if the service is running in windows using WSL

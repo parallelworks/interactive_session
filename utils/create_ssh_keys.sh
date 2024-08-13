2ed3a369a01b10e598c3a5848644e98a024988d9
Louis Le <lle@parallelworks.com>
2024-08-13 17:45:24 -0400

    test

--- utils/create_ssh_keys.sh
+++ utils/create_ssh_keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if ! [ -f "${HOME}/.ssh/id_rsa" ]; then
-    ssh-keygen -t rsa -N "" -q -f  ${HOME}/.ssh/id_rsa
+if ! [ -f "${HOME}/.ssh/pw_id_rsa" ]; then
+    ssh-keygen -t rsa -N "" -q -f  ${HOME}/.ssh/pw_id_rsa
 fi

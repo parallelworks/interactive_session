0eb69eb4e5c6e11950c0e6e1fa931caf9f4b5996
Louis Le <lle@parallelworks.com>
2024-08-14 10:04:38 -0400

    create pw_id_rsa and update config file

--- utils/create_ssh_keys.sh
+++ utils/create_ssh_keys.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+config_file=$1
+
 if ! [ -f "${HOME}/.ssh/pw_id_rsa" ]; then
-    ssh-keygen -t rsa -N "" -q -f  ${HOME}/.ssh/pw_id_rsa
+    ssh-keygen -t rsa -N "" -q -f ${HOME}/.ssh/pw_id_rsa
 fi
+
+# Modify the ssh config file for Host usercontainer
+sed -i '/Host usercontainer/{:a;n;/IdentityFile/s/id_rsa/pw_id_rsa/;Ta}' "$config_file"

e67c7b06563e9ed115953fc49e303bcef88cede2
avidalto <avidalto@hawk.iit.edu>
2024-09-11 04:58:42 -0500

    TEsting

--- ngencerf/start-template.sh
+++ ngencerf/start-template.sh
@@ -20,7 +20,7 @@ server {
  add_header X-Frame-Options "ALLOWALL";
  client_max_body_size 1000M;
  location / {
-     proxy_pass http://127.0.0.1:${service_existing_port};
+     proxy_pass http://127.0.0.1:${service_existing_port}/me/${openPort}/;
      proxy_http_version 1.1;
        proxy_set_header Upgrade \$http_upgrade;
        proxy_set_header Connection "upgrade";

e0d7bd8b3517e7a240b5e8bc5d492e1fd99a2663
avidalto <avidalto@hawk.iit.edu>
2024-09-23 06:56:05 -0500

    Start desktop inside xstartup

--- turbovnc/start-template.sh
+++ turbovnc/start-template.sh
@@ -93,6 +93,42 @@ echo
 
 
 if ! [[ $kernel_version == *microsoft* ]]; then
+    echo; echo "DESKTOP ENVIRONMENT"
+    if ! [ -z "${service_desktop}" ]; then
+        true
+    elif  ! [ -z $(which gnome-session) ]; then
+        gsettings set org.gnome.desktop.session idle-delay 0
+        service_desktop=gnome-session
+    elif ! [ -z $(which mate-session) ]; then
+        service_desktop=mate-session
+    elif ! [ -z $(which xfce4-session) ]; then
+        service_desktop=xfce4-session
+    elif ! [ -z $(which icewm-session) ]; then
+        # FIXME: Code below fails to launch desktop session
+        #        Use case in onyx automatically launches the session when visual apps are launched
+        service_desktop=icewm-session
+    elif ! [ -z $(which gnome) ]; then
+        service_desktop=gnome
+    else
+        # Exit script here
+        #displayErrorMessage "ERROR: No desktop environment was found! Tried gnome-session, mate-session, xfce4-session and gnome"
+        # The lines below do not run
+        echo "WARNING: vnc desktop not found!"
+        echo "Attempting to install a desktop environment"
+        # Following https://owlhowto.com/how-to-install-xfce-on-centos-7/
+        # Install EPEL release
+        sudo -n yum install epel-release -y
+        # Install Window-x system
+        sudo -n yum groupinstall "X Window system" -y
+        # Install XFCE
+        sudo -n yum groupinstall "Xfce" -y
+        if ! [ -z $(which xfce4-session) ]; then
+            displayErrorMessage "ERROR: No desktop environment was found! Tried gnome-session, mate-session, xfce4-session and gnome"
+        fi
+        # Start GUI
+        service_desktop=xfce-session
+    fi
+
 
     # This is only required for turbovnc:
     # https://turbovnc.org/Documentation/Compatibility30
@@ -131,9 +167,13 @@ if ! [[ $kernel_version == *microsoft* ]]; then
         echo 'unset SESSION_MANAGER' >> ~/.vnc/xstartup
         echo 'unset DBUS_SESSION_BUS_ADDRESS' >> ~/.vnc/xstartup
         echo '/etc/X11/xinit/xinitrc' >> ~/.vnc/xstartup
-	chmod +x ~/.vnc/xstartup
+        chmod +x ~/.vnc/xstartup
     fi
     
+    if ! grep -q gnome-session turbovnc/start-template.sh; then
+        echo "${service_desktop} &" >> ~/.vnc/xstartup
+    fi
+
     # service_vnc_type needs to be an input to the workflow in the XML
     # if vncserver is not tigervnc
     if [[ ${service_vnc_type} == "turbovnc" ]]; then
@@ -157,48 +197,10 @@ if ! [[ $kernel_version == *microsoft* ]]; then
     chmod og+rx /run/user/$(id -u)
     chmod 755 /run/user/$(id -u)/dconf
 
-    if ! [ -z "${service_desktop}" ]; then
-        eval ${service_desktop} &
-        echo $! > ${resource_jobdir}/service.pid
-    elif  ! [ -z $(which gnome-session) ]; then
-        gsettings set org.gnome.desktop.session idle-delay 0
-        gnome-session &
-        echo $! >> ${resource_jobdir}/service.pid
-    elif ! [ -z $(which mate-session) ]; then
-        mate-session &
-        echo $! >> ${resource_jobdir}/service.pid
-    elif ! [ -z $(which xfce4-session) ]; then
-        xfce4-session &
-        echo $! >> ${resource_jobdir}/service.pid
-    elif ! [ -z $(which icewm-session) ]; then
-        # FIXME: Code below fails to launch desktop session
-        #        Use case in onyx automatically launches the session when visual apps are launched
-        echo Found icewm-session
-        #icewm-session &
-        #echo $! > ${resource_jobdir}/service.pid
-    elif ! [ -z $(which gnome) ]; then
-        gnome &
-        echo $! >> ${resource_jobdir}/service.pid
-    else
-        # Exit script here
-        #displayErrorMessage "ERROR: No desktop environment was found! Tried gnome-session, mate-session, xfce4-session and gnome"
-        # The lines below do not run
-        echo "WARNING: vnc desktop not found!"
-        echo "Attempting to install a desktop environment"
-        # Following https://owlhowto.com/how-to-install-xfce-on-centos-7/
-        # Install EPEL release
-        sudo -n yum install epel-release -y
-        # Install Window-x system
-        sudo -n yum groupinstall "X Window system" -y
-        # Install XFCE
-        sudo -n yum groupinstall "Xfce" -y
-        if ! [ -z $(which xfce4-session) ]; then
-            displayErrorMessage "ERROR: No desktop environment was found! Tried gnome-session, mate-session, xfce4-session and gnome"
-        fi
-        # Start GUI
-        xfce4-session &
-        echo $! >> ${resource_jobdir}/service.pid
-    fi
+    # Start desktop here too just in case
+    eval ${service_desktop} &
+    echo "$!" >> ${resource_jobdir}/service.pid
+
 fi
 
 cd ${service_novnc_install_dir}

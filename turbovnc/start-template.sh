ef01cc28b4b102a3ebe1fe88201cc952dcbfc390
avidalto <avidalto@hawk.iit.edu>
2024-09-24 08:27:43 -0500

    Simply restart gnome-session over and over

--- turbovnc/start-template.sh
+++ turbovnc/start-template.sh
@@ -1,45 +1,12 @@
 # Make sure no conda environment is activated! 
 # https://github.com/parallelworks/issues/issues/1081
 
+
+
 start_gnome_session_with_retries() {
-    local max_retries=5
-    local retry_count=0
-
-    # Function to check if the session is already running on the global DISPLAY
-    is_session_running() {
-        for pid in $(ps -ef | grep gnome-session-binary | grep -v grep | awk '{print $2}'); do
-            display=$(cat /proc/${pid}/environ | tr '\0' '\n' | grep DISPLAY | cut -d'=' -f2)
-            if [[ "${DISPLAY}" == "${display}" ]]; then
-                return 0
-            fi
-        done
-        return 1
-    }
-
-    # First check if the session is already running
-    if is_session_running; then
-        echo "A gnome-session is already running on display $DISPLAY."
-        return 0
-    fi
-
-    # Attempt to start the service if it's not running
     while true; do
-        gnome-session &  # Start the session
-        service_desktop_pid=$!
-        sleep $((2+retry_count))  # Wait for a short moment to let the command attempt to start
-
-        # Check if the session is now running
-        if is_session_running; then
-            echo "gnome-session started successfully on display $DISPLAY."
-            break  # Exit the loop if the service started
-        else
-            echo "Failed to start gnome-session on display $DISPLAY. Retrying..."
-            ((retry_count++))
-            if [ $retry_count -ge $max_retries ]; then
-                echo "Reached maximum retries. Exiting."
-                return 1  # Exit the function with a non-zero status
-            fi
-        fi
+        gnome-session
+        sleep 2
     done
 }
 
@@ -240,7 +207,8 @@ if ! [[ $kernel_version == *microsoft* ]]; then
 
     # Start desktop here too just in case
     if [[ ${service_desktop} == "gnome-session" ]]; then
-        start_gnome_session_with_retries
+        start_gnome_session_with_retries &
+        service_desktop_pid=$!
     else
         eval ${service_desktop} &
         service_desktop_pid=$!

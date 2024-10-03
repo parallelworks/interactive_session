acbd8a419825655efebfeb0a096916540d877230
avidalto <avidalto@hawk.iit.edu>
2024-10-03 11:56:54 -0500

    Modify jupyter notebook to only use sithe singularity nginx proxy

--- jupyter-host/controller.sh
+++ jupyter-host/controller.sh
@@ -155,6 +155,7 @@ jupyter notebook --version
 
 if [ "${jupyter_major_version}" -ge 7 ]; then
     if [ -f "${service_nginx_sif}" ]; then
+        echo; echo "Downloading nginx singularity from Github"
         download_singularity_container
     fi
 fi

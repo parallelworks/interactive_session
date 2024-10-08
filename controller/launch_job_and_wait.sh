59b4fca009d94b8a069b36e47c897079e20203de
avidalto <avidalto@hawk.iit.edu>
2024-10-08 09:55:31 -0500

    fix stream

--- controller/launch_job_and_wait.sh
+++ controller/launch_job_and_wait.sh
@@ -21,8 +21,8 @@ if [[ "${use_screen}" == "true" ]]; then
     # Launch job
     screen_session_name="${workflow_name}-${job_number}"
     echo "Submitting session with the following command"
-    echo "$sshcmd screen -dmS ${screen_session_name} bash -c \"${resource_jobdir}/session-${job_number}.sh &> logs.out\""
-    $sshcmd screen -dmS ${screen_session_name} bash -c "${resource_jobdir}/session-${job_number}.sh &> logs.out"
+    echo "$sshcmd screen -dmS ${screen_session_name} bash -c \"${resource_jobdir}/session-${job_number}.sh &> ${resource_jobdir}/logs.out\""
+    $sshcmd screen -dmS ${screen_session_name} bash -c "${resource_jobdir}/session-${job_number}.sh &> ${resource_jobdir}/logs.out"
 
     # Prepare cleanup script
     echo "screen -X -S ${screen_session_name} quit" >> ${kill_ssh}

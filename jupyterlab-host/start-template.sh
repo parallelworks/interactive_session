5a926605ac442ef691cd7f06b67482f4f26f07b7
avidalto <avidalto@hawk.iit.edu>
2024-10-09 08:05:14 -0500

    generalizing jupyter install

--- jupyterlab-host/start-template.sh
+++ jupyterlab-host/start-template.sh
@@ -1,6 +1,21 @@
 # Runs via ssh + sbatch
 set -x
 
+if [ -z ${service_parent_install_dir} ]; then
+    service_parent_install_dir=${HOME}/pw/software
+fi
+
+if [ -z "${service_nginx_sif}" ]; then
+    service_nginx_sif=${service_parent_install_dir}/nginx-unprivileged.sif
+fi
+
+if [ -z "${service_load_env}" ]; then
+    service_conda_sh=${service_parent_install_dir}/${service_conda_install_dir}/etc/profile.d/conda.sh
+    service_load_env="source ${service_conda_sh}; conda activate ${service_conda_env}"
+fi
+
+eval "${service_load_env}"
+
 # Initialize cancel script
 echo '#!/bin/bash' > cancel.sh
 chmod +x cancel.sh

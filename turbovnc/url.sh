3c6f4018c30d9113e5314664e235ec9630cbb74e
avidalto <avidalto@hawk.iit.edu>
2024-11-12 08:09:26 -0600

    Converting desktop

--- turbovnc/url.sh
+++ turbovnc/url.sh
@@ -1,1 +1,3 @@
 export URLEND="vnc.html?resize=remote\&autoconnect=true\&show_dot=true\&path=websockify\&password=headless\&host=\"+window.location.host+\"/me/${openPort}\"+\"\/\&dt=\"+(new Date()).getTime()"
+
+slug="vnc.html?resize=remote\&autoconnect=true\&show_dot=true\&path=websockify\&password=headless\&host=\"+window.location.host+\"/me/${openPort}\"+\"\/\&dt=\"+(new Date()).getTime()"

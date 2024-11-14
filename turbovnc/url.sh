3f4b252c219f52bd985f9ef8df17d701450c9ea8
avidalto <avidalto@hawk.iit.edu>
2024-11-14 11:22:26 -0600

    test

--- turbovnc/url.sh
+++ turbovnc/url.sh
@@ -1,3 +1,1 @@
 export URLEND="vnc.html?resize=remote\&autoconnect=true\&show_dot=true\&path=websockify\&password=headless\&host=\"+window.location.host+\"/me/${openPort}\"+\"\/\&dt=\"+(new Date()).getTime()"
-
-slug="vnc.html?resize=remote\&autoconnect=true\&show_dot=true\&path=websockify\&password=headless\&host=\"+window.location.host+\"/me/${openPort}\"+\"\/\&dt=\"+(new Date()).getTime()"

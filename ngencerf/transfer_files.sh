7a88f7e2a9b509a1e5affaf30037911e1dbd1d94
avidalto <avidalto@hawk.iit.edu>
2024-11-18 06:02:56 -0600

    converting ngencerf to v3

--- ngencerf/transfer_files.sh
+++ ngencerf/transfer_files.sh
@@ -1,5 +1,5 @@
 if [ -z ${service_parent_install_dir} ]; then
-    service_parent_install_dir=${HOME}/pw/software
+    service_parent_install_dir=${resource_workdir}/pw/software
 fi
 
 rsync -avzq --rsync-path="mkdir -p ${service_parent_install_dir} && rsync" ${pw_job_dir}/${service_name}/slurm-wrapper-app.py ${resource_publicIp}:${resource_jobdir}

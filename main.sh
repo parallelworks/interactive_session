7d4d2dcddeefd24a025d884f5bfe3542f87fc6dd
avidalto <avidalto@hawk.iit.edu>
2024-11-19 09:08:17 -0600

    debug

--- main.sh
+++ main.sh
@@ -16,6 +16,7 @@ trap cleanup EXIT
 ./utils/steps/prepare_service_json.sh 2>&1 | tee prepare_service_json.out || exit 1
 ./utils/steps/initialize_cancel_script.sh 2>&1 | tee initialize_cancel_script.out || exit 1
 ./utils/steps/create_session_script.sh 2>&1 | tee create_session_script.out || exit 1
+set -x
 ./utils/steps/launch_job_and_wait.sh 2>&1 | tee launch_job_and_wait.out
 
 exit_code=${PIPESTATUS[0]}  # Captures the exit code of the first command

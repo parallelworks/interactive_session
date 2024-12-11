4e2780350a83f39de74246f994c89b50b3f88f66
avidalto <avidalto@hawk.iit.edu>
2024-12-11 06:59:45 -0600

    Fix bug with job 10

--- utils/steps-v3/preprocessing/initialize_cancel_script.sh
+++ utils/steps-v3/preprocessing/initialize_cancel_script.sh
@@ -20,13 +20,13 @@ cat utils/kill_session.sh >> ${kill_ssh}
 
 # KILL_SH: File that runs on the user space
 job_number_to_clean=$((job_number_int-10))
-clean_jobs="/pw/jobs/${workflow_name}/${job_number_to_clean}"
+job_to_clean="/pw/jobs/${workflow_name}/*${job_number_to_clean}"
 
 echo "#!/bin/bash" > ${kill_sh}
 echo "mv ${kill_sh} ${kill_sh}.completed" >> ${kill_sh}
 cat resources/host/inputs.sh >> ${kill_sh}
 if [ "${job_number_to_clean}" -gt 0 ]; then
-    echo "trap \"rm -rf ${clean_jobs}\" EXIT" >> ${kill_sh}
+    echo "trap \"rm -rf ${job_to_clean}\" EXIT" >> ${kill_sh}
 fi
 
 echo "echo Running ${kill_sh}" >> ${kill_sh}

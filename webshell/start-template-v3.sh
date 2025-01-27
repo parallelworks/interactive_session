196dd25d98e700d73e40bf047f58b818474cd869
avidalto <avidalto@hawk.iit.edu>
2025-01-27 08:01:13 -0600

    trying screen

--- webshell/start-template-v3.sh
+++ webshell/start-template-v3.sh
@@ -34,7 +34,7 @@ cd ~/
 
 rm -rf ${PWD}/service.pid
 
-${service_novnc_install_dir}/ttyd.x86_64 -p $service_port -s 0 bash &
+${service_novnc_install_dir}/ttyd.x86_64 -p $service_port screen -S persistent-session bash &
 echo $! >> ${PWD}/service.pid
 
 sleep 99999

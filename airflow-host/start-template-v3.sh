2be944c9cc69636c949b9104e8f15d93063c324f
avidalto <avidalto@hawk.iit.edu>
2025-03-06 08:27:57 -0600

    Add airflow

--- airflow-host/start-template-v3.sh
+++ airflow-host/start-template-v3.sh
@@ -36,17 +36,18 @@ server {
  add_header 'Access-Control-Allow-Headers' 'Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since';
  add_header X-Frame-Options "ALLOWALL";
  client_max_body_size 1000M;
- location ${basepath}/ {
-    proxy_pass http://${HOSTNAME}:${airflow_port};
-    proxy_set_header Host \$http_host;
-    proxy_redirect off;
-    proxy_http_version 1.1;
-    proxy_set_header Upgrade \$http_upgrade;
-    proxy_set_header Connection "upgrade";
-    proxy_set_header X-Real-IP \$remote_addr;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto \$scheme;
+
+ location / {
+     proxy_pass http://127.0.0.1:${airflow_port}${basepath}/;
+     proxy_http_version 1.1;
+       proxy_set_header Upgrade \$http_upgrade;
+       proxy_set_header Connection "upgrade";
+       proxy_set_header X-Real-IP \$remote_addr;
+       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+       proxy_set_header Host \$http_host;
+       proxy_set_header X-NginX-Proxy true;
  }
+
 }
 HERE
 

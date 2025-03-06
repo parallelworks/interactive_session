dbd7205a046e13e189ee5177d73d2dd6a526e342
avidalto <avidalto@hawk.iit.edu>
2025-03-06 07:46:55 -0600

    Add airflow

---
+++ airflow-host/start-template-v3.sh
@@ -0,0 +1,112 @@
+# Runs via ssh + sbatch
+set -x
+
+if [ -z ${service_parent_install_dir} ]; then
+    service_parent_install_dir=${HOME}/pw/software
+fi
+
+if [ -z "${service_nginx_sif}" ]; then
+    service_nginx_sif=${service_parent_install_dir}/nginx-unprivileged.sif
+fi
+
+echo "Activating Airflow environment"
+export AIRFLOW_HOME=${service_airflow_home}
+service_conda_install_dir=${service_parent_install_dir}/miniconda3-$(basename ${service_airflow_home})
+source ${service_conda_install_dir}/bin/activate
+
+# Initialize cancel script
+echo '#!/bin/bash' > cancel.sh
+chmod +x cancel.sh
+
+airflow_port=$(findAvailablePort)
+
+#######################
+# START NGINX WRAPPER #
+#######################
+
+echo "Starting nginx wrapper on service port ${service_port}"
+
+# Write config file
+cat >> config.conf <<HERE
+server {
+ listen ${service_port};
+ server_name _;
+ index index.html index.htm index.php;
+ add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+ add_header 'Access-Control-Allow-Headers' 'Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since';
+ add_header X-Frame-Options "ALLOWALL";
+ client_max_body_size 1000M;
+ location / {
+     proxy_pass http://127.0.0.1:${airflow_port}${basepath}/;
+     proxy_http_version 1.1;
+       proxy_set_header Upgrade \$http_upgrade;
+       proxy_set_header Connection "upgrade";
+       proxy_set_header X-Real-IP \$remote_addr;
+       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+       proxy_set_header Host \$http_host;
+       proxy_set_header X-NginX-Proxy true;
+ }
+}
+HERE
+
+cat >> nginx.conf <<HERE
+worker_processes  2;
+
+error_log  /var/log/nginx/error.log notice;
+pid        /tmp/nginx.pid;
+
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    proxy_temp_path /tmp/proxy_temp;
+    client_body_temp_path /tmp/client_temp;
+    fastcgi_temp_path /tmp/fastcgi_temp;
+    uwsgi_temp_path /tmp/uwsgi_temp;
+    scgi_temp_path /tmp/scgi_temp;
+
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile        on;
+    #tcp_nopush     on;
+
+    keepalive_timeout  65;
+
+    #gzip  on;
+
+    include /etc/nginx/conf.d/*.conf;
+}
+HERE
+
+echo "Running singularity container ${service_nginx_sif}"
+# We need to mount $PWD/tmp:/tmp because otherwise nginx writes the file /tmp/nginx.pid 
+# and other users cannot use the node. Was not able to change this in the config.conf.
+mkdir -p ./tmp
+# Need to overwrite default configuration!
+touch empty
+singularity run -B $PWD/tmp:/tmp -B $PWD/config.conf:/etc/nginx/conf.d/config.conf -B $PWD/nginx.conf:/etc/nginx/nginx.conf -B empty:/etc/nginx/conf.d/default.conf ${service_nginx_sif} >> nginx.logs 2>&1 &
+pid=$!
+echo "kill ${pid}" >> cancel.sh
+
+
+#################
+# START AIRFLOW #
+#################
+base_url=http://localhost/${basepath}
+sed -i "s|^base_url .*|base_url = ${BASE_URL}|" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s|^enable_proxy_fix .*|enable_proxy_fix = True|" ${AIRFLOW_HOME}/airflow.cfg
+
+airflow standalone
+
+
+sleep inf

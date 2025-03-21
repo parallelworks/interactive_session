bb0ef4d5b9bfa7a1bae668ddd34c94474cc8d319
avidalto <avidalto@hawk.iit.edu>
2025-03-21 11:24:12 -0500

    Testing

--- kasmvnc/start-template-v3.sh
+++ kasmvnc/start-template-v3.sh
@@ -17,13 +17,103 @@ chmod +x cancel.sh
 
 set -x
 
-if [ -z ${service_novnc_parent_install_dir} ]; then
-    service_novnc_parent_install_dir=${HOME}/pw/software
+if [ -z ${service_parent_install_dir} ]; then
+    service_parent_install_dir=${HOME}/pw/software
+fi
+
+if [ -z "${service_nginx_sif}" ]; then
+    service_nginx_sif=${service_parent_install_dir}/nginx-unprivileged.sif
 fi
 
 if [ -z "${service_port}" ]; then
     displayErrorMessage "ERROR: No service port found in the range \${minPort}-\${maxPort} -- exiting session"
 fi
+
+
+#######################
+# START NGINX WRAPPER #
+#######################
+
+export kasmvnc_websocket_port=$(findAvailablePort)
+
+echo "Starting nginx wrapper on service port ${service_port}"
+
+session_name="session${RANDOM}${RANDOM}"
+echo "Session Name: ${session_name}"
+server_name="${pw_user}-${session_name}.activate.pw"
+# Write config file
+cat >> config.conf <<HERE
+server {
+ listen ${service_port};
+ server_name ${server_name};
+ index index.html index.htm index.php;
+ add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+ add_header 'Access-Control-Allow-Headers' 'Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since';
+ add_header X-Frame-Options "ALLOWALL";
+ client_max_body_size 1000M;
+ location / {
+     proxy_pass http://0.0.0.0:${kasmvnc_websocket_port}/;
+     proxy_http_version 1.1;
+       proxy_set_header Upgrade \$http_upgrade;
+       proxy_set_header Connection "upgrade";
+       proxy_set_header X-Real-IP \$remote_addr;
+       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+       proxy_set_header Host \$http_host;
+       proxy_set_header X-NginX-Proxy true;
+       proxy_cache_bypass \$http_upgrade;
+       proxy_read_timeout 86400;
+ }
+}
+HERE
+
+cat >> nginx.conf <<HERE
+worker_processes  2;
+
+error_log  /var/log/nginx/error.log notice;
+pid        /tmp/nginx.pid;
+
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    proxy_temp_path /tmp/proxy_temp;
+    client_body_temp_path /tmp/client_temp;
+    fastcgi_temp_path /tmp/fastcgi_temp;
+    uwsgi_temp_path /tmp/uwsgi_temp;
+    scgi_temp_path /tmp/scgi_temp;
+
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile        on;
+    #tcp_nopush     on;
+
+    keepalive_timeout  65;
+
+    #gzip  on;
+
+    include /etc/nginx/conf.d/*.conf;
+}
+HERE
+
+echo "Running singularity container ${service_nginx_sif}"
+# We need to mount $PWD/tmp:/tmp because otherwise nginx writes the file /tmp/nginx.pid 
+# and other users cannot use the node. Was not able to change this in the config.conf.
+mkdir -p ./tmp
+# Need to overwrite default configuration!
+touch empty
+singularity run -B $PWD/tmp:/tmp -B $PWD/config.conf:/etc/nginx/conf.d/config.conf -B $PWD/nginx.conf:/etc/nginx/nginx.conf -B empty:/etc/nginx/conf.d/default.conf ${service_nginx_sif} >> nginx.logs 2>&1 &
+pid=$!
+echo "kill ${pid}" >> cancel.sh
 
 
 MAX_RETRIES=5
@@ -38,7 +128,7 @@ while ! is_kasmvnc_installed && [ $attempt -lt $MAX_RETRIES ]; do
     sleep $RETRY_INTERVAL
     attempt=$((attempt+1))
     # Disable ssl
-    # sudo sed -i 's/require_ssl: true/require_ssl: false/g' /usr/share/kasmvnc/kasmvnc_defaults.yaml
+    sudo sed -i 's/require_ssl: true/require_ssl: false/g' /usr/share/kasmvnc/kasmvnc_defaults.yaml
 done
 
 if ! is_kasmvnc_installed; then
@@ -51,7 +141,6 @@ if ! groups $USER | grep -q "\bkasmvnc-cert\b"; then
     echo "User is not in kasmvnc-cert group. Adding..."
     sudo usermod -a -G kasmvnc-cert $USER
     echo "Running newgrp to apply group changes..."
-    export service_port=${service_port}
     env > env.sh
     newgrp kasmvnc-cert
     source env.sh
@@ -102,13 +191,13 @@ expect -c 'spawn vncpasswd -u '"${USER}"' -w -r; expect "Password:"; send "'"${s
 
 
 vncserver -kill ${DISPLAY}
-echo "vncserver -kill ${DISPLAY}" >> cancel.sh
+echo "vncserver -kill ${DISPLAY}" >> cancel.sh.sh
 
 MAX_RETRIES=5
 RETRY_DELAY=5
 RETRY_COUNT=0
 
-vncserver_cmd="vncserver ${DISPLAY} ${disableBasicAuth} -select-de gnome -websocketPort ${service_port} -rfbport ${displayPort}"
+vncserver_cmd="vncserver ${DISPLAY} ${disableBasicAuth} -select-de gnome -websocketPort ${kasmvnc_websocket_port} -rfbport ${displayPort}"
 echo Running:
 echo ${vncserver_cmd}
 while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do

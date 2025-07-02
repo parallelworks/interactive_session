09d0247139de32935d9bbd15fcbc5e738c5f51ee
Matthew Shaxted <mattshax@gmail.com>
2025-07-02 15:55:55 -0500

    Committing kasm-proxy

---
+++ kasmvnc-proxy/url.sh
@@ -0,0 +1,1 @@
+export URLEND="?resize=remote\&autoconnect=true\&show_dot=true\&path=websockify\&password=headless\&host=\"+window.location.host+\"/me/${openPort}\"+\"\/\&dt=\"+(new Date()).getTime()"

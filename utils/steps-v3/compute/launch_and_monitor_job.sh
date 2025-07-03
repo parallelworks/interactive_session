8bda6a037dcd2c6caf48ada61d39ac9c85557082
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-07-03 18:30:57 +0200

    Add retries

--- utils/steps-v3/compute/launch_and_monitor_job.sh
+++ utils/steps-v3/compute/launch_and_monitor_job.sh
@@ -35,44 +35,81 @@ get_slurm_job_status() {
     # Get the header line to determine the column index corresponding to the job status
     if [ -z "${SQUEUE_HEADER}" ]; then
         export SQUEUE_HEADER="$(eval "$sshcmd ${status_cmd}" | awk 'NR==1')"
+        if [ $? -ne 0 ]; then
+            return 1  # Indicate failure
+        fi
     fi
     status_column=$(echo "${SQUEUE_HEADER}" | awk '{ for (i=1; i<=NF; i++) if ($i ~ /^S/) { print i; exit } }')
     status_response=$(eval $sshcmd ${status_cmd} | awk -v jobid="${jobid}" '$1 == jobid')
+    if [ $? -ne 0 ]; then
+        return 1  # Indicate failure
+    fi
     echo "${SQUEUE_HEADER}"
     echo "${status_response}"
     export job_status=$(echo ${status_response} | awk -v id="${jobid}" -v col="$status_column" '{print $col}')
+    return 0
 }
 
 get_pbs_job_status() {
     # Get the header line to determine the column index corresponding to the job status
     if [ -z "${QSTAT_HEADER}" ]; then
         export QSTAT_HEADER="$(eval "$sshcmd ${status_cmd}" | awk 'NR==1')"
+        if [ $? -ne 0 ]; then
+            return 1  # Indicate failure
+        fi
     fi
     status_response=$(eval $sshcmd ${status_cmd} 2>/dev/null | grep "\<${jobid}\>")
     echo "${QSTAT_HEADER}"
     echo "${status_response}"
     export job_status="$(eval $sshcmd ${status_cmd} -f ${jobid} 2>/dev/null  | grep job_state | cut -d'=' -f2 | tr -d ' ')"
-
+    if [ $? -ne 0 ]; then
+        return 1  # Indicate failure
+    fi
+    return 0
 }
 
 
 # Job status file writen by remote script:
 while true; do
     sleep 15
+    max_retries=20 # 5 min
+    retry_count=0
+    success=0
+
+    while [ $retry_count -lt $max_retries ]; do
+        if [[ ${jobschedulertype} == "SLURM" ]]; then
+            get_slurm_job_status
+            if [ $? -eq 0 ]; then
+                success=1
+                break
+            fi
+            echo "SSH error: Failed to get SLURM job status (attempt $((retry_count + 1))/$max_retries)"
+        elif [[ ${jobschedulertype} == "PBS" ]]; then
+            get_pbs_job_status
+            if [ $? -eq 0 ]; then
+                success=1
+                break
+            fi
+            echo "SSH error: Failed to get PBS job status (attempt $((retry_count + 1))/$max_retries)"
+        fi
+        retry_count=$((retry_count + 1))
+        sleep 5  # Wait 5 seconds before retrying
+    done
+
+    if [ $success -eq 0 ]; then
+        echo "Failed to get job status after $max_retries attempts"
+        export job_status=""
+    fi
+
     # squeue won't give you status of jobs that are not running or waiting to run
     # qstat returns the status of all recent jobs
     if [[ ${jobschedulertype} == "SLURM" ]]; then
-        get_slurm_job_status
-        # If job status is empty job is no longer running
         if [ -z "${job_status}" ]; then
-            job_status=$($sshcmd sacct -j ${jobid}  --format=state | tail -n1)
+            job_status=$($sshcmd sacct -j ${jobid} --format=state 2>/dev/null | tail -n1)
             break
         fi
     elif [[ ${jobschedulertype} == "PBS" ]]; then
-        get_pbs_job_status
-        if [[ "${job_status}" == "C" ]]; then
-            break
-        elif [ -z "${job_status}" ]; then
+        if [[ "${job_status}" == "C" ]] || [ -z "${job_status}" ]; then
             break
         fi
     fi
@@ -82,3 +119,4 @@ done
 echo "Job status: ${job_status}"
 
 $sshcmd scontrol show job ${jobid} -dd
+$sshcmd sacct -j ${jobid}

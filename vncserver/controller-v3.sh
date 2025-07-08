075c0ed5aa6c3e0bcc250fc6e2451e7d3e130d5b
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-07-08 17:22:20 +0200

    Also download singularity container

--- vncserver/controller-v3.sh
+++ vncserver/controller-v3.sh
@@ -1,8 +1,13 @@
-if [ -z ${service_novnc_parent_install_dir} ]; then
-    service_novnc_parent_install_dir=${HOME}/pw/software
+if [ -z ${service_parent_install_dir} ]; then
+    service_parent_install_dir=${HOME}/pw/software
 fi
 
-download_and_install() {
+
+if [ -z "${service_nginx_sif}" ]; then
+    service_nginx_sif=${service_parent_install_dir}/nginx-unprivileged.sif
+fi
+
+download_and_install_novnc() {
     # 1. Clone the repository with --no-checkout
     export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null '
     # Needed for emed
@@ -11,25 +16,53 @@ download_and_install() {
 
     # 2. Navigate into the repository directory
     cd interactive_session
+    #git checkout download-dependencies
 
     # 3. Initialize sparse-checkout
     git sparse-checkout init
 
-    # 4. Configure sparse-checkout to include both desired paths
-    echo "downloads/jupyter/nginx-unprivileged.sif" > .git/info/sparse-checkout
+    # 4. Configure sparse-checkout to include only the desired directory
     service_novnc_tgz_repo_path="downloads/vnc/${service_novnc_tgz_basename}"
-    echo "${service_novnc_tgz_repo_path}" >> .git/info/sparse-checkout
+    echo "${service_novnc_tgz_repo_path}" > .git/info/sparse-checkout
 
     # 5. Perform the checkout
     git checkout
 
-    # 6. Extract files
-    cp downloads/jupyter/nginx-unprivileged.sif "${service_nginx_sif}"
-    tar -zxf "${service_novnc_tgz_repo_path}" -C "${service_novnc_parent_install_dir}"
+    # 6. Extract tgz
+    tar -zxf ${service_novnc_tgz_repo_path} -C ${service_parent_install_dir}
 
     # 7. Clean
     cd ../
     rm -rf interactive_session
+    
+}
+
+download_singularity_container() {
+    # 1. Clone the repository with --no-checkout
+    export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null '
+    # Needed for emed
+    git config --global --unset http.sslbackend
+    git clone --no-checkout https://github.com/parallelworks/interactive_session.git
+
+    # 2. Navigate into the repository directory
+    cd interactive_session
+    #git checkout download-dependencies
+
+    # 3. Initialize sparse-checkout
+    git sparse-checkout init
+
+    # 4. Configure sparse-checkout to include only the desired file
+    echo downloads/jupyter/nginx-unprivileged.sif > .git/info/sparse-checkout
+
+    # 5. Perform the checkout
+    git checkout
+
+    # 6. Extract tgz
+    cp downloads/jupyter/nginx-unprivileged.sif ${service_nginx_sif}
+
+    # 7. Clean
+    cd ../
+    rm -rf interactive_session    
 }
 
 displayErrorMessage() {
@@ -38,14 +71,20 @@ displayErrorMessage() {
 
 echo; echo
 
-mkdir -p ${service_novnc_parent_install_dir}
+mkdir -p ${service_parent_install_dir}
 
 service_novnc_tgz_stem=$(echo ${service_novnc_tgz_basename} | sed "s|.tar.gz||g" | sed "s|.tgz||g")
-service_novnc_install_dir=${service_novnc_parent_install_dir}/${service_novnc_tgz_stem}
+service_novnc_install_dir=${service_parent_install_dir}/${service_novnc_tgz_stem}
 
 if ! [ -d "${service_novnc_install_dir}" ]; then
     echo "Downloading and installing ${service_novnc_install_dir}"
-    download_and_install
+    download_and_install_novnc
+fi
+
+# Download singularity container
+if ! [ -f "${service_nginx_sif}" ]; then
+    echo; echo "Downloading nginx singularity from Github"
+    download_singularity_container
 fi
 
 if ! [ -d "${service_novnc_install_dir}" ]; then

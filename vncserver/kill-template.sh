a7fd9b013195305cdb39dbc1cb06d876d7d00bd7
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-07-09 09:53:50 +0200

    Testing

--- vncserver/kill-template.sh
+++ vncserver/kill-template.sh
@@ -3,9 +3,8 @@ if [[ ${jobschedulertype} == "CONTROLLER" ]]; then
     echo "Running ${resource_jobdir}/cancel.sh"
     bash ${resource_jobdir}/cancel.sh
 else
-    compute_node=$(cat ${resource_jobdir}/target.hostname)
+    compute_node=$(cat ${resource_jobdir}/target.hostname | sed "s/.cluster.local//g")
     # Running the ssh command directly is not working
-    echo "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${compute_node} ${resource_jobdir}/cancel.sh" > ${resource_jobdir}/cancel_ssh.sh
-    bash ${resource_jobdir}/cancel_ssh.sh
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${compute_node} ${resource_jobdir}/cancel.sh
 fi
 

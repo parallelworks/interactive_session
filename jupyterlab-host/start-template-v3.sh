0463fa339e41e0fe4136c4603c46497a877f3c82
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-07-18 10:20:23 +0200

    Make suggested changes

--- jupyterlab-host/start-template-v3.sh
+++ jupyterlab-host/start-template-v3.sh
@@ -185,8 +185,15 @@ if [[ "${juice_use_juice}" == "true" ]]; then
         juice_exec=${service_parent_install_dir}/juice/juice
         echo "INFO: Set Juice executable path to ${juice_exec}"
     fi
-    juice_cmd="${juice_exec} ${juice_cmd_args} run"
-    echo "INFO: Prepared Juice command: ${juice_exec}"
+    
+    if ! [ -z "${juice_vram}" ]; then
+        vram_arg="--vram ${juice_vram}"
+    fi
+    if ! [ -z "${juice_pool_ids}" ]; then
+        pool_ids_arg="--pool-ids ${juice_pool_ids}"
+    fi
+    juice_cmd="${juice_exec} run ${juice_cmd_args} ${vram_arg} ${pool_ids_arg}"
+    echo "INFO: Prepared Juice command: ${juice_cmd}"
     echo "INFO: Logging into Juice with provided token"
     ${juice_exec} login -t "${JUICE_TOKEN}" || {
         echo "ERROR: Failed to log into Juice"

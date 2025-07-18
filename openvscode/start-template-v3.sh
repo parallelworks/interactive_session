f8383a12b227bd9d3d646718bd942883ac367509
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-07-18 11:28:52 +0200

    Add juice to code server

--- openvscode/start-template-v3.sh
+++ openvscode/start-template-v3.sh
@@ -53,10 +53,33 @@ pkill \${service_pid}
 HERE
 
 
-# START SERVICE
-echo ${service_exec} --bind-addr=${HOSTNAME}:${service_port} ${gh_flag} ${password_flag} ${service_directory}
+# JUICE https://docs.juicelabs.co/docs/juice/intro
+if [[ "${juice_use_juice}" == "true" ]]; then
+    echo "INFO: Enabling Juice for remote GPU access"
+    if [ -z "${juice_exec}" ]; then
+        juice_exec=${service_parent_install_dir}/juice/juice
+        echo "INFO: Set Juice executable path to ${juice_exec}"
+    fi
+    
+    if ! [ -z "${juice_vram}" ]; then
+        vram_arg="--vram ${juice_vram}"
+    fi
+    if ! [ -z "${juice_pool_ids}" ]; then
+        pool_ids_arg="--pool-ids ${juice_pool_ids}"
+    fi
+    juice_cmd="${juice_exec} run ${juice_cmd_args} ${vram_arg} ${pool_ids_arg}"
+    echo "INFO: Prepared Juice command: ${juice_cmd}"
+    echo "INFO: Logging into Juice with provided token"
+    ${juice_exec} login -t "${JUICE_TOKEN}" || {
+        echo "ERROR: Failed to log into Juice"
+        exit 1
+    }
+fi
 
-${service_exec} \
+# START SERVICE
+echo ${juice_cmd} ${service_exec} --bind-addr=${HOSTNAME}:${service_port} ${gh_flag} ${password_flag} ${service_directory}
+
+${juice_cmd} ${service_exec} \
     --bind-addr=${HOSTNAME}:${service_port} \
     ${gh_flag} \
     ${password_flag} \

a540eacd28cf8f2d2fd89da3c7444ed115525137
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-10-21 11:56:40 +0200

    Use singularity if vncserver is not installed

--- vncserver/controller-v3.sh
+++ vncserver/controller-v3.sh
@@ -7,6 +7,11 @@ if [ -z "${service_nginx_sif}" ]; then
     service_nginx_sif=${service_parent_install_dir}/nginx-unprivileged.sif
 fi
 
+if [ -z "${service_vncserver_sif}" ]; then
+    service_vncserver_sif=${service_parent_install_dir}/vncserver.sif
+fi
+
+
 download_and_install_novnc() {
     # 1. Clone the repository with --no-checkout
     export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null '
@@ -38,6 +43,8 @@ download_and_install_novnc() {
 }
 
 download_singularity_container() {
+    local repo_path=$1
+    local host_path=$2
     # 1. Clone the repository with --no-checkout
     export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null '
     # Needed for emed
@@ -52,13 +59,13 @@ download_singularity_container() {
     git sparse-checkout init
 
     # 4. Configure sparse-checkout to include only the desired file
-    echo downloads/jupyter/nginx-unprivileged.sif > .git/info/sparse-checkout
+    echo ${repo_path} > .git/info/sparse-checkout
 
     # 5. Perform the checkout
     git checkout
 
     # 6. Extract tgz
-    cp downloads/jupyter/nginx-unprivileged.sif ${service_nginx_sif}
+    mv ${repo_path} ${host_path}
 
     # 7. Clean
     cd ../
@@ -81,14 +88,31 @@ if ! [ -d "${service_novnc_install_dir}" ]; then
     download_and_install_novnc
 fi
 
-# Download singularity container
+# Download nginx singularity container
 if ! [ -f "${service_nginx_sif}" ]; then
     echo; echo "Downloading nginx singularity from Github"
-    download_singularity_container
+    download_singularity_container downloads/jupyter/nginx-unprivileged.sif ${service_nginx_sif}
 fi
 
 if ! [ -d "${service_novnc_install_dir}" ]; then
     echo
     displayErrorMessage "Failed to install ${service_novnc_install_dir}"
     exit 1
+fi
+
+# Download vnserver container if vncserver is missing
+if [[ "${HOSTNAME}" == gaea* && -f /usr/lib/vncserver ]]; then
+    export service_vnc_exec=/usr/lib/vncserver
+fi
+
+if [ -z "${service_vnc_exec}" ]; then
+    service_vnc_exec=$(which vncserver)
+fi
+
+if [ -z "${service_vnc_exec}" ] || ! [ -f "${service_vnc_exec}" ]; then
+    if ! which singularity > /dev/null 2>&1; then
+        echo "(date) ERROR: No vncserver or singularity command found"
+        exit 1
+    fi
+    download_singularity_container downloads/vnc/vncserver.sif ${service_vncserver_sif}
 fi

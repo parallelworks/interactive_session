76917bf255c2108df3c6c59bf899152acdf05097
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-10-21 12:34:09 +0200

    Make sure directory exists

--- vncserver/start-template-v3.sh
+++ vncserver/start-template-v3.sh
@@ -125,6 +125,8 @@ if [ -z ${service_vnc_exec} ] || ! [ -f "${service_vnc_exec}" ]; then
     echo "$(date): vncserver is not installed. Using singularity container..."
     service_vnc_exec="singularity exec --bind /tmp/.X11-unix:/tmp/.X11-unix --bind ${HOME}:${HOME} ${service_vncserver_sif}"
     service_vnc_type="TurboVNC"
+    mkdir -p /tmp/.X11-unix
+    chmod 1777 /tmp/.X11-unix
 fi
 
 if [ -z ${service_vnc_type} ]; then

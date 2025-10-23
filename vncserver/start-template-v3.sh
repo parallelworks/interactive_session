d9771ae3243a6c880304dba18072fd0b7d660a7a
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-10-23 16:37:43 +0200

    testing

--- vncserver/start-template-v3.sh
+++ vncserver/start-template-v3.sh
@@ -131,7 +131,7 @@ if [ -z ${service_vnc_exec} ] || ! [ -f "${service_vnc_exec}" ]; then
         exit 1
     fi
     echo "$(date): vncserver is not installed. Using singularity container..."
-    service_vnc_exec="singularity exec --writable-tmpfs --bind /tmp/.X11-unix:/tmp/.X11-unix --bind ${HOME}:${HOME} ${service_vncserver_sif} vncserver -xstartup ~/.vnc/xstartup.turbovnc"
+    service_vnc_exec="singularity exec --writable-tmpfs --bind /tmp/.X11-unix:/tmp/.X11-unix --bind ${HOME}:${HOME} --bind /etc/machine-id:/etc/machine-id --bind /var/lib/dbus/machine-id:/var/lib/dbus/machine-id ${service_vncserver_sif} vncserver -xstartup ~/.vnc/xstartup.turbovnc"
     service_vnc_type="TurboVNC"
     service_desktop="echo Starting no service desktop on the host"
     mkdir -p /tmp/.X11-unix

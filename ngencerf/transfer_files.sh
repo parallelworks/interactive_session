fb671a985e70db6f46d51205f97579f786df225c
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2025-11-05 12:19:08 +0100

    Fix typo

--- ngencerf/transfer_files.sh
+++ ngencerf/transfer_files.sh
@@ -1,7 +1,9 @@
+set -e
+
 if [ -z ${service_parent_install_dir} ]; then
     service_parent_install_dir=${resource_workdir}/pw/software
 fi
 
 rsync  -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -avzq --rsync-path="mkdir -p ${service_parent_install_dir} && rsync" ${pw_job_dir}/${service_name}/slurm-wrapper-app-v3.py ${resource_publicIp}:${resource_jobdir}
-rsync  -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -avzq --rsync-path="mkdir -p ${service_parent_install_dir} && rsync" ${pw_job_dir}/${service_name}/run_calback.sh ${resource_publicIp}:${resource_jobdir}
-rsync  -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -avzq --rsync-path="mkdir -p ${service_parent_install_dir} && rsync" ${pw_job_dir}/${service_name}/rerun_calbacks.sh ${resource_publicIp}:${resource_jobdir}
+rsync  -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -avzq --rsync-path="mkdir -p ${service_parent_install_dir} && rsync" ${pw_job_dir}/${service_name}/run_callback.sh ${resource_publicIp}:${resource_jobdir}
+rsync  -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -avzq --rsync-path="mkdir -p ${service_parent_install_dir} && rsync" ${pw_job_dir}/${service_name}/rerun_callbacks.sh ${resource_publicIp}:${resource_jobdir}

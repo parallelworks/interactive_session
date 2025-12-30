f029b530f8ed14a57ba5bb6fe64893540af22a4a
Michael McQuade <michael@giraffesyo.io>
2025-12-30 12:12:41 -0600

    remove

--- utils/steps-v3/controller/launch_and_monitor_job.sh
+++ utils/steps-v3/controller/launch_and_monitor_job.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source utils/load-env.sh
-source resources/host/inputs.sh
 
 if [[ "${use_screen}" == "true" ]]; then
     scp -p ${session_sh} ${resource_publicIp}:${resource_jobdir}/session-${job_number}.sh

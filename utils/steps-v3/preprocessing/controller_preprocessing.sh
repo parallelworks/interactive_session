b7fba3ef981b1e5875024e2f81ae92a300de1857
Michael McQuade <michael@giraffesyo.io>
2025-12-30 12:04:13 -0600

    comment this out

--- utils/steps-v3/preprocessing/controller_preprocessing.sh
+++ utils/steps-v3/preprocessing/controller_preprocessing.sh
@@ -5,7 +5,7 @@ set -xeo pipefail
 # Runs the <service-name>/controller-v3.sh script in the controller node which is
 # used to install software or run other speficic steps in the controller
 source utils/load-env.sh
-source resources/host/inputs.sh
+# source resources/host/inputs.sh
 
 [[ "${DEBUG:-}" == "true" ]] && set -x
 

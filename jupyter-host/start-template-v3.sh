345e6445b249b719e5caa548d99c43b68afd1f1a
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-01-12 11:29:40 +0100

    Fix override

--- jupyter-host/start-template-v3.sh
+++ jupyter-host/start-template-v3.sh
@@ -12,7 +12,7 @@ fi
 # Always compute the correct conda paths based on install directories
 service_conda_sh=${service_parent_install_dir}/${service_conda_install_dir}/etc/profile.d/conda.sh
 # Always set service_load_env to use the correct path (override any pre-set value)
-if [[ "${service_conda_install}" == "true" ]]; then
+if [[ "${service_conda_install}" == "true" ]] && [ -z "${service_load_env}" ]; then
     service_load_env="source ${service_conda_sh}; conda activate ${service_conda_env}"
 fi
 

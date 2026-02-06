83ba4b337cda28f1e234c994ceb99bb78c1b16a5
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-02-06 17:53:40 +0100

    Update start-template-v3.sh (#651)

--- jupyterlab-host/start-template-v3.sh
+++ jupyterlab-host/start-template-v3.sh
@@ -272,6 +272,8 @@ fi
 
 date
 
+mkdir -p ${resource_jobdir}/jupyter_runtime
+export JUPYTER_RUNTIME_DIR=${resource_jobdir}/jupyter_runtime
 ${juice_cmd} jupyter-lab --port=${jupyterlab_port} --no-browser --config=${resource_jobdir}/jupyter_lab_config.py --allow-root
 #jupyter-lab --port=${jupyterlab_port} --ip ${HOSTNAME} --no-browser --config=${PWD}/jupyter_lab_config.py
 

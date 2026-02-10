4133864d32dfb32347454029b2a5fd5eb5cc8c82
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-02-10 10:37:51 +0100

    Update start-template-v3.sh

--- webshell/start-template-v3.sh
+++ webshell/start-template-v3.sh
@@ -7,32 +7,6 @@ fi
 service_novnc_tgz_stem=$(echo ${service_novnc_tgz_basename} | sed "s|.tar.gz||g" | sed "s|.tgz||g")
 service_novnc_install_dir=${service_parent_install_dir}/${service_novnc_tgz_stem}
 
-# ----- REMOVE AFTER V4 IS RELEASED EVERYWHERE ----- #
-# Prepare kill service script
-# - Needs to be here because we need the hostname of the compute node.
-# - kill-template.sh --> service-kill-${job_number}.sh --> service-kill-${job_number}-main.sh
-
-if [[ ${jobschedulertype} == "CONTROLLER" ]]; then
-    echo "bash ${PWD}/service-kill-${job_number}-main.sh" > service-kill-${job_number}.sh
-else
-    # Remove .cluster.local for einteinmed!
-    hname=$(hostname | sed "s/.cluster.local//g")
-    echo "ssh ${hname} 'bash -s' < ${PWD}/service-kill-${job_number}-main.sh" > service-kill-${job_number}.sh
-fi
-
-screen_name=webterm${service_port}
-cat >> service-kill-${job_number}-main.sh <<HERE
-service_pid=\$(cat ${PWD}/service.pid)
-if [ -z \${service_pid} ]; then
-    echo "ERROR: No service pid was found!"
-else
-    echo "$(hostname) - Killing process: ${service_pid}"
-    pkill -P \${service_pid}
-    kill \${service_pid}
-fi
-HERE
-# ----- REMOVE AFTER V4 IS RELEASED EVERYWHERE ----- #
-
 cd ~/
 
 rm -rf ${PWD}/service.pid

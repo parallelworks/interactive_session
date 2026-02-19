2c4c137e4bfadea2db645b57c7d660c7d2801e82
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-02-19 11:19:14 +0100

    Explicitly mount home directory

--- kasmvnc-singularity/start-template-v3.sh
+++ kasmvnc-singularity/start-template-v3.sh
@@ -106,6 +106,7 @@ singularity run \
     --bind /etc/environment:/etc/environment:ro \
     --bind $PWD/empty:/etc/nginx/conf.d/default.conf \
     --bind $PWD/error.log:/var/log/nginx/error.log \
+    --bind ${HOME}/.ssh \
     "${container_dir}" &
 
 kasmvnc_container_pid=$!

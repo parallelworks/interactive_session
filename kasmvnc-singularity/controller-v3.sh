460d7ea08400bd16866c22bfc0530aea8d512bb3
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-03-12 16:36:47 +0100

    Fix desktop (#743)
    * Ensure PW_PLATFORM_HOST does not include https
    
    * Attempt to install xterm if missing

--- kasmvnc-singularity/controller-v3.sh
+++ kasmvnc-singularity/controller-v3.sh
@@ -43,9 +43,13 @@ if ! [ -d "${container_dir}" ]; then
     rm ${container_tgz}
 fi
 
-xterm_path=$(which xterm)
-if ! [ -z ${xterm_path} ]; then
-    cp ${xterm_path} ${service_parent_install_dir}/xterm
-    chmod +x ${service_parent_install_dir}/xterm || true
-fi
 
+xterm_path=$(which xterm 2>/dev/null)
+if [ -z "${xterm_path}" ]; then
+    sudo -n dnf install xterm -y 2>/dev/null || true
+    xterm_path=$(which xterm 2>/dev/null)
+fi
+if [ -n "${xterm_path}" ]; then
+    cp ${xterm_path} ${service_parent_install_dir}/xterm
+    chmod +x ${service_parent_install_dir}/xterm
+fi

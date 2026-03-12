738e3d7c5cf02c98e7031baf07c09719242d6f2e
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-03-12 16:34:53 +0100

    Attempt to install xterm if missing

--- kasmvnc-docker/controller-v3.sh
+++ kasmvnc-docker/controller-v3.sh
@@ -7,8 +7,12 @@ fi
 
 mkdir -p ${service_parent_install_dir}
 
-xterm_path=$(which xterm)
-if ! [ -z ${xterm_path} ]; then
+xterm_path=$(which xterm 2>/dev/null)
+if [ -z "${xterm_path}" ]; then
+    sudo -n dnf install xterm -y 2>/dev/null || true
+    xterm_path=$(which xterm 2>/dev/null)
+fi
+if [ -n "${xterm_path}" ]; then
     cp ${xterm_path} ${service_parent_install_dir}/xterm
     chmod +x ${service_parent_install_dir}/xterm
 fi

a81fea52858ed991072358c60c3a9054b8520e57
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-04-16 16:18:53 +0200

    Hsp shared directory (#813)
    * Configure hsp to use a shared install directory
    
    * Do not attempt to install oras if already there
    
    * Prioritize the value of the service_parent_install_dir in the session YAML
    
    * Prioritize the value of the service_parent_install_dir in the session YAML

--- kasmvnc-singularity/controller-v3.sh
+++ kasmvnc-singularity/controller-v3.sh
@@ -1,15 +1,25 @@
 set -o pipefail
 set -x
 
-if [ -z ${service_parent_install_dir} ]; then
+
+if [ -n "${service_parent_install_dir}" ]; then
+    container_dir=${service_parent_install_dir}/kasmvnc-${kasmvnc_os}
+    if ! [ -d "${container_dir}" ] && ! [ -w "${service_parent_install_dir}" ]; then
+        echo "::warning::container_dir ${container_dir} does not exist and no write permission to ${service_parent_install_dir}. Resetting to ${HOME}/pw/software."
+        service_parent_install_dir=${HOME}/pw/software
+    fi
+else
     service_parent_install_dir=${HOME}/pw/software
 fi
 
-container_tgz=${service_parent_install_dir}/kasmvnc-${kasmvnc_os}.tgz
 container_dir=${service_parent_install_dir}/kasmvnc-${kasmvnc_os}
+container_tgz=${container_dir}.tgz
 
 download_oras(){
-    VER="1.2.0"   # example — replace with newest                                                                                    
+    if [ -d "${service_parent_install_dir}/oras" ]; then
+        return
+    fi
+    VER="1.2.0"   # example — replace with newest
     wget https://github.com/oras-project/oras/releases/download/v${VER}/oras_${VER}_linux_amd64.tar.gz
     mkdir -p ${service_parent_install_dir}/oras
     tar -xvf oras_${VER}_linux_amd64.tar.gz -C ${service_parent_install_dir}/oras

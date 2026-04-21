8833b8cd55e02a1423795bbcd25064434a0cd5ee
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-04-21 10:31:03 +0200

    Update controller-v3.sh (#820)

--- kasmvnc-singularity/controller-v3.sh
+++ kasmvnc-singularity/controller-v3.sh
@@ -40,10 +40,12 @@ oras_pull_file(){
     repo=$1
     repo_path=$2
     host_path=$3
-    ${service_parent_install_dir}/tools/oras/oras pull ${repo}
+    if ! ${service_parent_install_dir}/tools/oras/oras pull ${repo}; then
+        echo "::error title=Error::oras pull failed for ${repo}"
+        exit 1
+    fi
     mv ${repo_path} ${host_path}
 }
-
 
 # The reason we need service_download_vncserver_container is:
 # - vncserver can be installed in the compute nodes but not in the controlle nodes

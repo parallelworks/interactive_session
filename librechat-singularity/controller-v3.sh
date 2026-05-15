ac85aae20492995bbfe4dd32d9f72612865d7a60
Alvaro Vidal Torreira <alvaro@parallelworks.com>
2026-05-15 16:30:27 +0200

    Add default config with GenAI MIL and Activate endpoints

--- librechat-singularity/controller-v3.sh
+++ librechat-singularity/controller-v3.sh
@@ -99,6 +99,7 @@ endpoints:
       apiKey: "\${PW_API_KEY}"
       baseURL: "https://${PW_PLATFORM_HOST}/api/openai/v1"
       models:
+        default: []
         fetch: true
       titleConvo: true
       summarize: false
